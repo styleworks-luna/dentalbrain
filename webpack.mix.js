f0c2054d7fde3aba4299edaa71d3dd9c22a77eca
muyi221 <muyi221@onoffmix.com>
2020-10-19 15:00:26 +0900

    [ADD] 공통 scss 추가

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.sass', 'public/css');
+
+mix.sass('resources/sass/pages/index.sass', 'public/css/pages');
