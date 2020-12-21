5a3ff8fd80cfecdd1c1ec3f15c9a20542e9fac8f
chdaud78 <qkrri56@naver.com>
2020-12-21 18:00:18 +0900

    [ENH] MICE-1911 header, footer 작업

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,35 @@
 <footer class="footer">
-    <div class="footer-wrap"></div>
+    <div class="container">
+            <div class="footer-logo-wrap">
+                <a href="" class="footer-logo ir_pm">logo</a>
+            </div>
+            <div class="footer-left">
+                <ul>
+                    <li>
+                        <p>상호 : 주식회사 브레인스펙병원교육개발원 </p>
+                    </li>
+                    <li>
+                        <p>대표 : 김민정</p>
+                    </li>
+                    <li>
+                        <p>등록번호 : 114-87-09709</p>
+                    </li>
+                    <li>
+                        <p>사업장 소재지 : 서울특별시 서초구 효령로 140 (방배동,3층)</p>
+                    </li>
+                    <li>
+                        <p>TEL : 070-8222-3179</p>
+                    </li>
+                </ul>
+                <div class="office-address">© BRAINSPEC. ALL RIGHTS RESERVED.</div>
+            </div>
+            <div class="footer-right">
+                <ul>
+                    <li><a href="" class="footer-right-brainspec"></a></li>
+                    <li><a href="" class="footer-right-ohc"></a></li>
+                    <li><a href="" class="footer-right-kadh"></a></li>
+                    <li><a href="" class="footer-right-kdma"></a></li>
+                </ul>
+            </div>
+        </div>
 </footer>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,29 @@
 <header class="header">
-    <div class="header-wrap"></div>
+    <div class="container">
+            <div class="header-logo-wrap">
+                <a href="" class="ir_pm header-logo">logo</a>
+            </div>
+            <div class="login-menu">
+                <p class="user-name"><em>홍길동</em> 님</p>
+                <a href="" class="login-btn">로그아웃</a>
+            </div>
+            <div class="header-left">
+                <ul>
+                    <li><a href="">회사소개</a></li>
+                    <li><a href="">강의안내</a></li>
+                    <li><a href="">강사소개</a></li>
+                    <li><a href="">전체강의</a></li>
+                </ul>
+            </div>
+            <div class="header-right">
+                <ul>
+                    <li>
+                        <a href="" class="header-mypage">마이페이지</a>
+                    </li>
+                    <li>
+                        <a href="" class="header-cs">고객센터</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
 </header>
