84d5e218a1a4c64195cb02bde1abe8547ed3272e
ndsrhkd <ndsrhkd@onoffmix.com>
2021-01-05 17:56:34 +0900

    [ENH] MICE-1969 회원가입 생성

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
@@ -44,30 +45,36 @@ class RegisterController extends Controller
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     * @param array $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'login_id' => ['required', 'string', 'min:4', 'max:255', 'unique:users'],
+            'name' => ['required', 'string', 'min:2', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'password' => ['required', 'string', 'min:6', 'max:40', 'confirmed'],
+            'job' => ['required', 'min:0', 'max:5'],
+            'phone' => ['required'],
         ]);
     }
 
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
+     * @param array $data
      * @return User
      */
     protected function create(array $data)
     {
         return User::create([
+            'login_id' => $data['login_id'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'phone' => $data['phone'],
+            'api_token' => Str::random(80),
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,34 +13,38 @@
 
 Route::get('/', function () {
     return view('desktop.index');
-});
+})->name('index');
 
 //회사 소개 (임시)
-Route::get('introduce',function () {
+Route::get('introduce', function () {
     return view('desktop.pages.introduce.about_us');
 });
 
 //강사 소개 (임시)
-Route::get('instructor',function () {
+Route::get('instructor', function () {
     return view('desktop.pages.introduce.instructor');
 });
 
 //강의 상세 (임시)
-Route::get('lecture',function () {
+Route::get('lecture', function () {
     return view('desktop.pages.lecture.lecture_detail');
 });
 
 //강의 신청 (임시)
-Route::get('apply',function () {
+Route::get('apply', function () {
     return view('desktop.pages.lecture.lecture_apply');
 });
 
 //마이페이지 신청한 강의 (임시)
-Route::get('mypage',function () {
+Route::get('mypage', function () {
     return view('desktop.pages.user.mypage_lecture');
 });
 
-//회원가입 (임시)
-Route::get('register',function () {
-    return view('desktop.pages.user.register');
+Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
+    //회원가입 (임시)
+    Route::get('register', function () {
+        return view('desktop.pages.user.register');
+    })->name('register');
+
+    Route::post('create', 'Auth\RegisterController@register')->name('create');
 });
