be310a5fcf495802360f443505233695eb8d115e
ndsrhkd <ndsrhkd@onoffmix.com>
2021-01-06 09:52:56 +0900

    [DEL] MICE-1970 테스트 코드 삭제

--- app/Http/Middleware/StoreAgentToSession.php
+++ app/Http/Middleware/StoreAgentToSession.php
@@ -16,12 +16,13 @@ class StoreAgentToSession
      */
     public function handle($request, Closure $next)
     {
-        $agent = new Agent();
-        if ($agent->isMobile()) {
-            $request->session()->put(['agent' => 'mobile']);
-        } else {
-            $request->session()->put(['agent' => 'desktop']);
-        }
+//        $agent = new Agent();
+//        if ($agent->isMobile()) {
+//            $request->session()->put(['agent' => 'mobile']);
+//        } else {
+//            $request->session()->put(['agent' => 'desktop']);
+//        }
+        $request->session()->put(['agent' => 'desktop']);
 
         return $next($request);
     }
