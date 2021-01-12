ae7ffb16dbe1795521537b414f50aa879f8616a2
sunggun1 <sunggun1@onoffmix.com>
2021-01-12 16:41:55 +0900

    [mod] MICE-1983
    - 이름 서로 바뀌어서 이름 변경

--- app/Interfaces/ViewCount/ViewCountImpl.php
+++ app/Interfaces/ViewCount/ViewCountImpl.php
@@ -1,8 +1,23 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: onoffmix
+ * Date: 2021-01-12
+ * Time: 오후 3:19
+ */
 namespace App\Interfaces\ViewCount;
 
+use App\Interfaces\ViewCount\ViewCountMethod;
 use Illuminate\Database\Eloquent\Model;
-interface ViewCountMethod {
-    public function viewCountAdd(string $modelName,int $id);
+use Illuminate\Support\Facades\DB;
+
+class ViewCountImpl extends Model implements ViewCountMethod{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function viewCountAdd(string $modelName , int $id){
+        DB::table($modelName)->where('id',$id)->increment('views');
+    }
 }
-?>
