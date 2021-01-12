a7ece4d702b5ad8773a8894b17e85a28ca567352
sunggun1 <sunggun1@onoffmix.com>
2021-01-12 17:56:04 +0900

    [mod] MICE-1983
    - notice 에 글쓴이 관리자로 나오도록 변경

--- resources/views/desktop/pages/service/notice.blade.php
+++ resources/views/desktop/pages/service/notice.blade.php
@@ -24,7 +24,7 @@
                             <li class="notice-content">
                                 <p class="index list-common">{{$value -> id }}</p>
                                 <a href="/notice/detail/{{$value -> id}}" class="title list-common">{{ $value -> title }}</a>
-                                <p class="writer list-common">{{$value -> display_name }}</p>
+                                <p class="writer list-common">{{$value -> name }}</p>
                                 <p class="date list-common">{{$value -> created_at }}</p>
                                 <p class="views list-common">{{$value-> views }}</p>
                             </li>
