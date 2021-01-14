86eb18abb0fe5732f33d51b46b447b04c412a825
muyi221 <muyi221@onoffmix.com>
2021-01-14 17:06:40 +0900

    [ADD] MICE-1984 관리자, grid componets 추가

---
+++ resources/js/components/admin/grid/Layout.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <h1 class="float-left mb-0 pt-2 pb-2 font-xl">{{ title }}</h1>
+
+                    <div class="float-right">
+                        <slot name="button"></slot>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <slot name="page"></slot>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AdminLayout',
+        props: {
+            title: String
+        }
+    }
+</script>
---
+++ resources/js/components/admin/grid/Table.vue
@@ -0,0 +1,37 @@
+<template>
+    <table class="table table-responsive-sm table-bordered text-center">
+        <thead>
+            <tr>
+                <th v-for="col in tableCol"
+                    :key="col.name">
+                    {{ col.text }}
+
+                    <slot v-if="col.isSort" :name="col.name"></slot>
+                </th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <template v-if="data.length > 0">
+                <tr v-for="row in data"
+                    :key="row.id">
+                    <slot name="list" :row="row"></slot>
+                </tr>
+            </template>
+
+            <tr class="empty-list" v-else>
+                <td :colspan="tableCol.length">데이터가 없습니다.</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: 'Table',
+        props: {
+            tableCol: Array,
+            data: Array
+        }
+    }
+</script>
