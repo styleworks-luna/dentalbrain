96f8e4d323702ec29a4e321935a4eaf25f6fc469
muyi221 <muyi221@onoffmix.com>
2021-01-15 14:24:34 +0900

    [ADD] MICE-1984 공통 component, 작업

---
+++ resources/js/components/common/SelectBox.vue
@@ -0,0 +1,31 @@
+<template>
+    <select @change="handleSetValue($event.target.value)">
+        <option value=""
+                v-if="!Helper.nullCheck(text)">{{ text }}</option>
+
+        <option v-for="(option, index) in options"
+                :value="option.id"
+                :selected="isSelected(option.id)">
+            {{ option.name }}
+        </option>
+    </select>
+</template>
+
+<script>
+    export default {
+        name: 'SelectBox',
+        props: {
+            'text': String,
+            'value': [Number, String],
+            'options': Array
+        },
+        methods: {
+            isSelected(value) {
+                return value == this.value;
+            },
+            handleSetValue(value) {
+                this.$emit('setValue', value);
+            }
+        }
+    }
+</script>
