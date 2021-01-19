e3314fa08eb602bdd2882d78792841c28d76e52e
chdaud78 <qkrri56@naver.com>
2021-01-19 10:18:46 +0900

    [ENH] MICE-1984 문의 내역 상세 보기 페이지 수정 불가 항목 추가

--- resources/js/views/admin/customer/InquireEdit.vue
+++ resources/js/views/admin/customer/InquireEdit.vue
@@ -1,19 +1,53 @@
 <template>
     <layout title="FAQ 수정">
         <template v-slot:body>
-<!--            <single-group> {{ this.email }}</single-group>
-            <single-group> {{ this.name }}</single-group>
-            <single-group> {{ this.phone }}</single-group>-->
-            <single-group name="구분"
-                          :isRow="true"
-                          :isRequired="true"
-                          :size="6">
+
+
+            <single-group  name="번호" :size="9" :isRow="true">
+                <template v-slot:content>
+                    {{ id }}
+                </template>
+            </single-group>
+
+            <single-group  name="작성일" :size="9" :isRow="true">
+                <template v-slot:content>
+                    {{ data.created_at }}
+                </template>
+            </single-group>
+
+            <single-group  name="이메일" :size="9" :isRow="true">
+                <template v-slot:content>
+                    {{ data.email }}
+                </template>
+            </single-group>
+
+            <single-group  name="이름" :size="9" :isRow="true">
+            <template v-slot:content>
+                    {{ data.name }}
+            </template>
+            </single-group>
+
+            <single-group  name="연락처" :size="9" :isRow="true">
+                <template v-slot:content>
+                    {{ data.phone }}
+                </template>
+            </single-group>
+
+            <single-group  name="문의내용" :size="9" :isRow="true">
+                <template v-slot:content>
+                    {{ data.content }}
+                </template>
+            </single-group>
+
+            <single-group  name="구분" :size="9" :isRow="true">
                 <template v-slot:content>
                     <select-box class="form-control"
                                 :value="category"></select-box>
                 </template>
             </single-group>
+
         </template>
+
         <template v-slot:footer>
             <div class="float-left">
                 <button type="button" class="btn btn-danger"
@@ -48,7 +82,33 @@ export default {
     data() {
         return {
             id: '',
-            data: {}
+            data: []
+        }
+    },
+    computed: {
+        tableCol() {
+            return [
+                {
+                    name: 'id',
+                    text: '번호'
+                },
+                {
+                    name: 'created_at',
+                    text: '작성일'
+                },
+                {
+                    name: 'email',
+                    text: '이메일'
+                },
+                {
+                    name: 'name',
+                    text: '이름'
+                },
+                {
+                    name: 'phone',
+                    text: '연락처'
+                },
+            ]
         }
     },
     created() {
@@ -59,16 +119,17 @@ export default {
     },
     methods: {
         getEditData() {
+
             Inquire.getEditData(this.id).then(res => {
-                console.log(res);
+                const result = res.data.inquiry;
+
+                this.data = result;
             });
+
         },
         update() {
             let data = {
-                question: this.question,
-                answer: this.answer,
-                category_id: this.category_id,
-                is_open: this.is_open
+                category: this.category,
             };
 
             Inquire.update(this.id, data).then(res => {
