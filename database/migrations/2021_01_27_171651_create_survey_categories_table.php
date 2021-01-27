4514cfe95f22f12d558406b4e75aeea6da31cffb
ndsrhkd <ndsrhkd@onoffmix.com>
2021-01-27 17:30:14 +0900

    [ADD] MICE-1986 강의 설문조사 DB 마이그레이션 추가

---
+++ database/migrations/2021_01_27_171651_create_survey_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSurveyCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_categories');
+    }
+}
