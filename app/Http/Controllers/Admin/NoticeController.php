27fc5ecb1c213bed0d2286ce50e3bba5dab81deb
sunggun1 <sunggun1@onoffmix.com>
2021-01-28 09:53:42 +0900

    [mod] MICE-2001
    - index에 들어간 경우 request에 keyword가 있거나 어떤 분류가 있으면 해당 검색값으로 받아옴

--- app/Http/Controllers/Admin/NoticeController.php
+++ app/Http/Controllers/Admin/NoticeController.php
@@ -19,11 +19,9 @@ class NoticeController extends Controller
         $this->search = new SearchService(Notice::query());
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $notice = Notice::whereNotNull('id')
-            ->orderByDesc('id')
-            ->paginate(10);
+        $notice = $this->search($request);
         return response()->json([
             'notice' => $notice,
         ]);
@@ -88,12 +86,12 @@ class NoticeController extends Controller
         return $statusChangeImpl->statusChange($notice, 'is_open');
     }
 
-    public function search(Request $request){
+    private function search(Request $request){
         $this->search
             ->addKeyword('title',$request->keyword)
             ->addKeyword('content',$request->keyword);
 
-        $result = $this->search->search()->paginate('10');
-        return response()->json(['search' =>$result]);
+        $result = $this->search->search()->orderBy('id','desc')->paginate('10');
+        return $result;
     }
 }
