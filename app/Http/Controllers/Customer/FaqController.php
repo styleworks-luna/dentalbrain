e5d6167c4dae057e5c38bf28e45dbe2d06ec67d2
sunggun1 <sunggun1@onoffmix.com>
2021-01-28 16:04:26 +0900

    [mod] MICE-2001
    - FaqController 오픈 된 것 만 최신순으로 정렬
    - cateogry_name 나오게 변경

--- app/Http/Controllers/Customer/FaqController.php
+++ app/Http/Controllers/Customer/FaqController.php
@@ -9,7 +9,7 @@ class FaqController extends Controller
 {
     public function index()
     {
-        $faqs = Faq::all()->where('is_open','1');
+        $faqs = Faq::where('is_open','1')->orderBy('id','desc')->get();
         return view(viewPrefix() . 'pages.service.faq', ['faqs' => $faqs]);
     }
 }
--- resources/views/desktop/pages/service/faq.blade.php
+++ resources/views/desktop/pages/service/faq.blade.php
@@ -24,7 +24,7 @@
                         @foreach($faqs as $key => $value)
                             <li class="faq-content">
                                 <div class="question-information">
-                                    <p class="sort">강의신청</p>
+                                    <p class="sort">{{$value->category_name}}</p>
                                     <div class="question">
                                         <em>Q</em>
                                         <a href="" class="question-content">{{ $value->question }}</a>
