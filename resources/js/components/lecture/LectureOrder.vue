37c4a43f8996893d0e728b74077fb31df5351dd1
chdaud78 <qkrri56@naver.com>
2021-02-04 15:24:13 +0900

    [ENH] MICE-2009 강의 전체 페이지 order css 변경

--- resources/js/components/lecture/LectureOrder.vue
+++ resources/js/components/lecture/LectureOrder.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="lecture-order">
         <ul>
-            <li><a href="">인기순</a></li>
             <li><a href="" class="active">최신순</a></li>
+            <li><a href="">인기순</a></li>
         </ul>
     </div>
 </template>
