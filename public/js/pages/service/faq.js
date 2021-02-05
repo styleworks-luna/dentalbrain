309fdb7db9be3c258e1431298f40402a1f0254a5
chdaud78 <qkrri56@naver.com>
2021-02-05 14:12:45 +0900

    [ENH] MICE-2009 FAQ 답변 접근성 추가

--- public/js/pages/service/faq.js
+++ public/js/pages/service/faq.js
@@ -1,9 +1,17 @@
 $(function () {
     //강의 상세보기
     var arrowDown = $('.arrow-down');
+    var question =  $('.question-content');
 
     arrowDown.click(function () {
         $(this).toggleClass('arrow-change');
         $(this).parent().parent().find('.answer').toggleClass('hide-show');
     })
+
+    question.click(function(e) {
+        e.preventDefault();
+        $(this).parent().parent().parent().find('.arrow-down').toggleClass('arrow-change');
+        $(this).parent().parent().parent().find('.answer').toggleClass('hide-show');
+    });
+
 });
