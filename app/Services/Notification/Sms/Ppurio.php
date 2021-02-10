6cdb5c07eefdb6603d52a415f830057aae1ca9f0
sunggun1 <sunggun1@onoffmix.com>
2021-02-10 16:20:04 +0900

    [MICE-1994] 뿌리오 토큰 가져오기

--- app/Services/Notification/Sms/Ppurio.php
+++ app/Services/Notification/Sms/Ppurio.php
@@ -1,34 +1,19 @@
 <?php
 
-$host = 'https://api.bizppurio.com/v1/token';
-//$host = 'https://dev-api.bizppurio.com/v1/token';
+namespace App\Services\Notification\Sms;
+use GuzzleHttp\Client;
 
-$headers = array(
-    'Accept: application/json',
-    'Content-Type:application/json',
-    'Authorization: Basic '. base64_encode("brainspec:well5511$$")
-);
+class Ppurio{
+    public function getToken() {
+        $client = new Client();
+        $authorize = base64_encode(env('PPURIO_ID').":".env('PPURIO_SECRET'));
+        $res = $client->request('POST', 'https://api.bizppurio.com/v1/token', [
+            'headers' => [
+                'Authorization' => 'Basic '.$authorize,
+                'Content-Type' => 'application/json',
+            ]
+        ]);
 
-$oCurl = curl_init();
-curl_setopt($oCurl, CURLOPT_URL, $host);
-curl_setopt($oCurl, CURLOPT_POST, true);
-curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($oCurl, CURLOPT_NOSIGNAL, 1);
-curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($oCurl, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($oCurl, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($oCurl, CURLOPT_VERBOSE, true);
-
-$response = curl_exec($oCurl);
-$curl_errno = curl_errno($oCurl);
-$curl_error = curl_error($oCurl);
-
-curl_close($oCurl);
-
-echo 'Response :';
-echo '<pre>';
-print_r(json_decode($response));
-print_r($curl_error);
-echo '</pre>';
-?>
+        return $res;
+    }
+}
