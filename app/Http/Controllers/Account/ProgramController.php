9bf5672faa940213c7e8c2d6ce58bd38cf1689de
sunggun1 <sunggun1@onoffmix.com>
2021-02-15 14:55:39 +0900

    [ENH] MICE-2016  마이페이지 신청한 강의 내역 json 타입으로 나오도록 변경

--- app/Http/Controllers/Account/ProgramController.php
+++ app/Http/Controllers/Account/ProgramController.php
@@ -3,11 +3,46 @@
 namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
+use App\Models\Program\Program;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\Program\ProgramStudent;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Builder;
 
 class ProgramController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view(viewPrefix() . 'pages.user.mypage.mypage_lecture');
+        $data = Program::select('id','is_online','title','running_time')
+        ->with([
+            'ticket',
+            'students' => function($query){
+                $query->select('user_id','expired_at');
+                $query->where('user_id',Auth::id());
+            }
+        ]);
+
+        $this->setOrder($data,$request->order);
+
+        return response()->json(['result' => $data->get()->toArray()]);
+    }
+
+
+    private function setOrder($data, $order){
+        switch($order){
+            case 'newest' :
+                $data->orderBy('id','desc');
+                break;
+            case 'online' :
+                $data->where('is_online','1');
+                break;
+            case 'offline' :
+                $data->where('is_online','0');
+                break;
+            default :
+                break;
+        }
+        return $data;
     }
 }
