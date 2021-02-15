e07d04d2c7f66824bebc16525fec69476ad95ea7
chdaud78 <qkrri56@naver.com>
2021-02-15 13:41:37 +0900

    [ENH] MICE-1964 마이페이지 신청한 강의 vue로 옮김

---
+++ resources/js/components/mypage/lecture/Lecture.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <lecture-order></lecture-order>
+    <lecture-list></lecture-list>
+    </div>
+</template>
+
+<script>
+import LectureList from '@/components/mypage/lecture/LectureList.vue';
+import LectureOrder from '@/components/mypage/lecture/LectureOrder.vue';
+
+export default {
+    name: 'MypageLecture',
+    components: {
+        'lecture-list': LectureList,
+        'lecture-order': LectureOrder,
+    },
+}
+</script>
---
+++ resources/js/components/mypage/lecture/LectureOrder.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="content-title">
+        <h2>신청한 강의</h2>
+        <ul>
+            <li class="active"><a href="">최신순</a></li>
+            <li><a href="">온라인</a></li>
+            <li><a href="">오프라인</a></li>
+        </ul>
+    </div>
+</template>
