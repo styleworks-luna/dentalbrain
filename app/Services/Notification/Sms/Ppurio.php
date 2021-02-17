57a50b0715e9d816c0f2d3f1b5cc7b96409499ca
sunggun1 <sunggun1@onoffmix.com>
2021-02-17 17:38:05 +0900

    [ENH] MICE-1994 뿌리오 문자 날리기 guzzlehttp 안쓰고 원래대로 해봄

--- app/Services/Notification/Sms/Ppurio.php
+++ app/Services/Notification/Sms/Ppurio.php
@@ -21,35 +21,65 @@ class Ppurio{
     public function checkVerification(Request $request){
         $client = new Client();
         $token = $this->getToken();
-        $content = json_encode([
-                "sms" => array(
-                    "message" => "Test"
-                )
-            ]
-        );
-        $resend = json_encode([
-            "first" => "sms"
-        ]);
-        $recontent = json_encode([
-            "sms" => array(
-                "message" => "SMS 대체 발송"
-            )
-        ]);
 
-        $result = $client->request('POST','https://api.bizppurio.com/v3/message',[
-           'headers'=>[
-               'Authorization' => $token['type']." ".$token['accesstoken'],
-               'Content-Type' => 'application/json',
-           ],
-            'form_params' =>[
-                'account' => env('PPURIO_ID'),
-                'type' => 'sms',
-                'from' => $request->phone,
-                'to' => $request->phone,
-                'content' => $content
-            ]
-        ]);
+        $sms = array("message" => "SMS 테스트");
+        $content = array("sms" => $sms);
 
-        return json_decode($result->getBody()->getContents(),true);
+        $data =array();
+        $data['account'] = env('PPURIO_ID');
+        $data['type'] = "sms";
+        $data['from'] = $request->phone;
+        $data['to'] = $request->phone;
+        $data['content'] = $content;
+
+        $json_data = json_encode($data,JSON_UNESCAPED_SLASHES);
+
+        $url = 'https://api.bizppurio.com/v3/message';
+
+        $oCurl = curl_init();
+        curl_setopt($oCurl,CURLOPT_URL,$url);
+        curl_setopt($oCurl, CURLOPT_NOSIGNAL, 1);
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($oCurl, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($oCurl, CURLOPT_HTTPHEADER, array('Accept:application/json','Content-Type:application/json','Authorization:'.$token['type']." ".$token['accesstoken']));
+        curl_setopt($oCurl, CURLOPT_VERBOSE, true);
+        curl_setopt($oCurl, CURLOPT_POSTFIELDS, $json_data);
+        curl_setopt($oCurl, CURLOPT_TIMEOUT, 3);
+
+        $response = curl_exec($oCurl);
+        $curl_errno = curl_errno($oCurl);
+        $curl_error = curl_error($oCurl);
+
+        curl_close($oCurl);
+        echo 'Response :';
+        echo '<pre>';
+        print_r(json_decode($response));
+        print_r($curl_error);
+        echo '</pre>';
+//
+//        $content = json_encode([
+//                "sms" => array(
+//                    "message" => "Test"
+//                )
+//            ]
+//        );
+//
+//        $result = $client->request('POST','https://api.bizppurio.com/v3/message',[
+//           'headers'=>[
+//               'Authorization' => $token['type']." ".$token['accesstoken'],
+//               'Content-Type' => 'application/json',
+//           ],
+//            'form_params' =>[
+//                'account' => env('PPURIO_ID'),
+//                'type' => 'sms',
+//                'from' => $request->phone,
+//                'to' => $request->phone,
+//                'content' => $content
+//            ]
+//        ]);
+//
+//        return json_decode($result->getBody()->getContents(),true);
+        return json_decode($response);
     }
 }
