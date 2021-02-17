023852a6832fd086361cdef5b3893d30eefad44d
sunggun1 <sunggun1@onoffmix.com>
2021-02-17 11:03:34 +0900

    [ENH] MICE-2016 order online,offline,newest에 따라서 값 다르게 나오도록 수정

--- app/Http/Controllers/Account/ProgramController.php
+++ app/Http/Controllers/Account/ProgramController.php
@@ -23,34 +23,33 @@ class ProgramController extends Controller
         $data = ProgramStudent::query()->select('id','user_id','payment_id','ticket_id','expired_at')
         ->with([
             'payment:id,totalAmount,receiptUrl,method,status',
-            'ticket.program' => function($query){
+            'ticket.program' => function($query) use ($request) {
                 $query->select('id','thumbnail_id','title','is_online','running_time','major_category_id','minor_category_id')
                 ->with('place:id,program_id,address,address_detail,sido,gugun,started_at,ended_at')
                 ->with('thumbnail:id,path,url');
             },
+        ])->whereHas('ticket.program',function($query) use($request) {
+            $query = $this->addOnlineorOffline($request->order,$query);
+        })->where('user_id','=',Auth::id());
 
-        ])->where('user_id','=',Auth::id())->get();
-
-        $data = $this->setOrder($data, $request->order);
+        $data = $this->setNewest($data, $request->input('order'));
         
-        return response()->json(['data'=> $data]);
+        return response()->json(['data'=> $data->paginate('10')]);
     }
 
+    private function addOnlineorOffline($order,$query){
+        if($order == 'online'){
+            $query->where('is_online','1');
+        }else if($order == 'offline'){
+            $query->where('is_online','0');
+        }
+        return $query;
+    }
 
-    private function setOrder($data, $order)
+    private function setNewest($data, $order)
     {
-        switch ($order) {
-            case 'newest' :
-                $data->orderBy('id', 'desc');
-                break;
-            case 'online' :
-                $data->where('is_online', '1');
-                break;
-            case 'offline' :
-                $data->where('is_online', '0');
-                break;
-            default :
-                break;
+        if($order == 'newest'){
+            $data->orderBy('id','desc');
         }
         return $data;
     }
