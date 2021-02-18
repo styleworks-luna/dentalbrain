84f9731bfad22930aa8b78637965dd17ae95fe7a
chdaud78 <qkrri56@naver.com>
2021-02-18 14:54:36 +0900

    [ENH] 강의 카드 온라인 오프라인 추가

--- app/Models/Program/Program.php
+++ app/Models/Program/Program.php
@@ -177,7 +177,7 @@ class Program extends Model
 
     public function scopePublic(Builder $query, $category, $orderBy)
     {
-        $programs = $query->select(['id', 'thumbnail_id', 'major_category_id', 'minor_category_id', 'title', 'running_time'])
+        $programs = $query->select(['id', 'thumbnail_id', 'is_online', 'major_category_id', 'minor_category_id', 'title', 'running_time'])
             ->where('is_open', '=', 1)
             ->with(['thumbnail:id,url', 'ticket:id,price,program_id,is_free','place:id,program_id,started_at,ended_at'])
             ->withCount('students');
