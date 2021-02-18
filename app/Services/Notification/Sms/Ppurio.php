e922fa7880dc2b6f7698d69ee10ceaf12466caa4
sunggun1 <sunggun1@onoffmix.com>
2021-02-18 10:21:34 +0900

    [ENH] MICE-1994 다른 값들 표출 안 되고 필요한 값만 return만 하도록 변경

--- app/Services/Notification/Sms/Ppurio.php
+++ app/Services/Notification/Sms/Ppurio.php
@@ -49,47 +49,9 @@ class Ppurio{
         $response = curl_exec($oCurl);
         $curl_errno = curl_errno($oCurl);
         $curl_error = curl_error($oCurl);
-        var_dump($content);
+
         curl_close($oCurl);
-        echo 'Response :';
-        echo '<pre>';
-        print_r(json_decode($response));
-        print_r($curl_error);
-        echo '</pre>';
-
-//        $client = new Client();
-//        $token = $this->getToken();
-
-//        $sms = array("message" => "SMS 테스트");
-//        $content = array("sms" => $sms);
-//
-//        $data =array();
-//        $data['account'] = env('PPURIO_ID');
-//        $data['refkey']=env('PPURIO_KEY');
-//        $data['type'] = "sms";
-//        $data['from'] = $request->phone;
-//        $data['to'] = $request->phone;
-//        $data['content'] = $content;
-//
-//        $json_data = json_encode($data,JSON_UNESCAPED_SLASHES);
-//
-////
-////        $content = json_encode([
-////                "sms" => array(
-////                    "message" => "Test"
-////                )
-////            ]
-////        );
-//
-//        $result = $client->request('POST','https://api.bizppurio.com/v3/message',[
-//           'headers'=>[
-//               'Authorization' => $token['type']." ".$token['accesstoken'],
-//               'Content-Type' => 'application/json',
-//           ],
-//            'form_params' => $json_data
-//        ]);
-//
-//        return json_decode($result->getBody()->getContents(),true);
+        
         return json_decode($response);
     }
 }
