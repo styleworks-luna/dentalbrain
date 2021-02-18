def2c47859ba2bec6e902d495dcc9a6bd3addae3
sunggun1 <sunggun1@onoffmix.com>
2021-02-18 15:38:29 +0900

    [ENH] MICE-1994 전화번호가 11자인데 10자밖에 저장이 안되서 변경

--- app/Http/Controllers/Notification/PhoneVerificationController.php
+++ app/Http/Controllers/Notification/PhoneVerificationController.php
@@ -12,7 +12,7 @@ class PhoneVerificationController extends Controller
 {
     public function checkVerification(Request $request){
         $validatedData = $request->validate([
-            'phone' => 'required|min:11|max:11'
+            'phone' => 'required|min:10|max:12'
         ]);
 
         $sms = new Ppurio();
@@ -21,8 +21,8 @@ class PhoneVerificationController extends Controller
 
     public function getVerificationNumber(Request $request){
         $validator = Validator::make($request->all(), [
-            'phone' => 'required|min:11|max:12',
-            'verficationNumber' => 'required|min:6|max:7'
+            'phone' => 'required|min:10|max:12',
+            'verficationNumber' => 'required|min:6|max:6'
         ]);
 
         if($validator->fails()){
