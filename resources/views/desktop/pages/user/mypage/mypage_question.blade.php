8ab397afc746907c2ffb74748bfaf47f4a40a70a
chdaud78 <qkrri56@naver.com>
2021-02-25 16:39:57 +0900

    [ENH] 질문내역 없을떄 css 수정

--- resources/views/desktop/pages/user/mypage/mypage_question.blade.php
+++ resources/views/desktop/pages/user/mypage/mypage_question.blade.php
@@ -41,7 +41,7 @@
                             </div>
                         </li>
                     @empty
-                        <li>질문내역이 없습니다.</li>
+                        <li class="history-content">질문내역이 없습니다.</li>
                     @endforelse
                 </ul>
             </section>
