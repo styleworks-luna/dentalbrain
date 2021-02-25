8547e4b20fe442776061cb2c2ba2a4825c135df0
sunggun1 <sunggun1@onoffmix.com>
2021-02-25 16:55:05 +0900

    [ENH] MICE-2025 질문 답변 상태 변경 함수 추가

--- routes/web.php
+++ routes/web.php
@@ -291,6 +291,8 @@ Route::group(['prefix' => 'api', 'as' => 'api.'], function () {
                 Route::get('{question}/edit','Admin\Program\QuestionController@edit')->name('edit');
                 // 질문 업데이트 함수
                 Route::post('{question}','Admin\Program\QuestionController@update')->name('update');
+                //질문 답변 변경 함수
+                Route::patch('{question}/status', 'Admin\Program\QuestionController@statusChange')->name('statusChange');
             });
         });
 
