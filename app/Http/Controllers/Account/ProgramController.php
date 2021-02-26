6a98fb5fe589c5050259ec1db09d13218a06ba6f
ndsrhkd <ndsrhkd@onoffmix.com>
2021-02-26 11:23:08 +0900

    [ENH] MICE-2015 마이페이지 신청한 강의 코드 수정

--- app/Http/Controllers/Account/ProgramController.php
+++ app/Http/Controllers/Account/ProgramController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
 use App\Models\Program\ProgramStudent;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class ProgramController extends Controller
 {
@@ -18,43 +18,36 @@ class ProgramController extends Controller
 
     public function lecturesData(Request $request)
     {
-        $data = ProgramStudent::query()->select('id', 'user_id', 'payment_id', 'ticket_id', 'expired_at')
+        $request->validate([
+            'order' => ['required', Rule::in(['online', 'offline', 'newest'])],
+        ]);
+
+        //TODO: 사용자에 따른 강의 구분 정보 넣기.
+        $queryBuilder = ProgramStudent::query()->select('id', 'user_id', 'payment_id', 'ticket_id', 'expired_at')
             ->with([
                 'payment:id,totalAmount,receiptUrl,method,status',
-                'ticket.program' => function ($query) use ($request) {
+                'ticket.program' => function ($query) {
                     $query->select('id', 'thumbnail_id', 'title', 'is_online', 'running_time', 'major_category_id', 'minor_category_id')
                         ->with('place:id,program_id,address,address_detail,sido,gugun,started_at,ended_at')
                         ->with('thumbnail:id,path,url')
                         ->with('lectures:id,program_id');
                 },
-            ])->whereHas('ticket.program', function ($query) use ($request) {
-                $query = $this->addWhereOnlineOrOffline($request->order, $query);
-            })->where('user_id', '=', Auth::id());
+                // 기본 정렬
+            ])->orderByDesc('id')
+            ->where('user_id', '=', Auth::id());
 
-        $data = $this->setNewest($request->input('order'), $data);
-        return response()->json(['data' => $data->paginate('10')]);
-    }
-
-    private function addWhereOnlineOrOffline($order, $query)
-    {
-        if ($order == 'online') {
-            $query->where('is_online', '1');
-        } else if ($order == 'offline') {
-            $query->where('is_online', '0');
+        if ($request->input('order', 'newest') == 'online') {
+            // 온라인 정렬일 경우.
+            $queryBuilder = $queryBuilder->whereHas('ticket.program', function ($query) {
+                $query->where('is_online', '1');
+            });
+        } elseif ($request->input('order', 'newest') == 'offline') {
+            // 오프라인 정렬일 경우.
+            $queryBuilder = $queryBuilder->whereHas('ticket.program', function ($query) {
+                $query->where('is_online', '0');
+            });
         }
-        return $query;
-    }
 
-    /**
-     * @param $order
-     * @param $query
-     * @return Builder
-     */
-    private function setNewest($order, $query)
-    {
-        if ($order == 'newest') {
-            $query->orderBy('id', 'desc');
-        }
-        return $query;
+        return response()->json(['data' => $queryBuilder->paginate('10')]);
     }
 }
