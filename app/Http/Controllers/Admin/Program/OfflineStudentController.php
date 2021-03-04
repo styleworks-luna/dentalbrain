c4ca651e1d917eddcefc604827d356f5673964ca
ndsrhkd <ndsrhkd@onoffmix.com>
2021-03-04 17:07:45 +0900

    [ENH] MICE-2017 중복 코드 관련 컨트롤러로 이전

--- app/Http/Controllers/Admin/Program/OfflineStudentController.php
+++ app/Http/Controllers/Admin/Program/OfflineStudentController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers\Admin\Program;
 
 use App\Models\Program\Program;
-use App\Models\Program\ProgramStudent;
-use App\Models\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class OfflineStudentController extends OfflineProgramController
 {
@@ -29,27 +26,4 @@ class OfflineStudentController extends OfflineProgramController
         ]);
     }
 
-    /**
-     * 어드민 환불 처리
-     *
-     * @param Request $request
-     * @param Program $program
-     * @param ProgramStudent $student
-     * @return JsonResponse
-     * @see OnlineStudentController @cancel
-     */
-    public function cancel(Request $request, Program $program, ProgramStudent $student)
-    {
-        $validatedData = $this->offlineConcrete->validateAdminCancel($request, $program, User::find($student->user_id));
-        if ($validatedData) {
-            $response = $this->offlineConcrete->cancel($program, $student, $validatedData);
-        } else {
-            return response()->json(['message' => '유효하지 않은 요청입니다.'], 422);
-        }
-
-        if ($response === false) {
-            return response()->json(['message' => '취소 오류 발생 하였습니다.'], 500);
-        }
-        return response()->json(['message' => '취소되었습니다.']);
-    }
 }
--- app/Http/Controllers/Admin/Program/OnlineStudentController.php
+++ app/Http/Controllers/Admin/Program/OnlineStudentController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers\Admin\Program;
 
 use App\Models\Program\Program;
-use App\Models\Program\ProgramStudent;
-use App\Models\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class OnlineStudentController extends OnlineProgramController
 {
@@ -27,29 +24,5 @@ class OnlineStudentController extends OnlineProgramController
             'program_name' => $program->title,
             'students' => $this->onlineConcrete->getStudents($program)
         ]);
-    }
-
-    /**
-     * 어드민 환불 처리
-     *
-     * @param Request $request
-     * @param Program $program
-     * @param ProgramStudent $student
-     * @return JsonResponse
-     * @see OfflineStudentController @cancel
-     */
-    public function cancel(Request $request, Program $program, ProgramStudent $student)
-    {
-        $validatedData = $this->onlineConcrete->validateAdminCancel($request, $program, User::find($student->user_id));
-        if ($validatedData) {
-            $response = $this->onlineConcrete->cancel($program, $student, $validatedData);
-        } else {
-            return response()->json(['message' => '유효하지 않은 요청입니다.'], 422);
-        }
-
-        if ($response === false) {
-            return response()->json(['message' => '취소 오류 발생 하였습니다.'], 500);
-        }
-        return response()->json(['message' => '취소되었습니다.']);
     }
 }
