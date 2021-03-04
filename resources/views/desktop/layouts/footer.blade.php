b52a1aea1ed816608ee71e02ab36bc6d93b2f1e2
chdaud78 <qkrri56@naver.com>
2021-03-04 09:26:13 +0900

    [ENH] footer url 추가

--- resources/views/desktop/layouts/footer.blade.php
+++ resources/views/desktop/layouts/footer.blade.php
@@ -31,22 +31,22 @@
             <div class="footer-right">
                 <ul>
                     <li>
-                        <a href="" class="footer-right-brainspec">
+                        <a href="http://www.brainspec.co.kr/" class="footer-right-brainspec">
                             <img src="{{ asset('/images/desktop/global/logo-brainspec.png') }}" alt="brainspec">
                         </a>
                     </li>
                     <li>
-                        <a href="" class="footer-right-ohc">
+                        <a href="https://smartstore.naver.com/ohcshop/" class="footer-right-ohc">
                             <img src="{{ asset('/images/desktop/global/logo-ohc.png') }}" alt="ohc 칫솔샵">
                         </a>
                     </li>
                     <li>
-                        <a href="" class="footer-right-kadh">
+                        <a href="http://www.kadh.or.kr/" class="footer-right-kadh">
                             <img src="{{ asset('/images/desktop/global/logo-kadh.png') }}" alt="KADH 대한치과위생학회">
                         </a>
                     </li>
                     <li>
-                        <a href="" class="footer-right-kdma">
+                        <a href="http://kmma.co.kr/" class="footer-right-kdma">
                             <img src="{{ asset('/images/desktop/global/logo-kdma.png') }}" alt="KDMA 대한치과경영관리협회">
                         </a>
                     </li>
