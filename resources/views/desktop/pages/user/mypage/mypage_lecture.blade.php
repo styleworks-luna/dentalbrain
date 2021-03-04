be33f3395f3dad2b7c4435f60ca4e7b48b6d0640
chdaud78 <qkrri56@naver.com>
2021-03-04 11:38:56 +0900

    [ENH] MICE-2017 마이페이지 신청한 강의 환불 요청 pop css 수정

--- resources/views/desktop/pages/user/mypage/mypage_lecture.blade.php
+++ resources/views/desktop/pages/user/mypage/mypage_lecture.blade.php
@@ -4,6 +4,20 @@
     <script type="text/javascript" src="{{ asset('js/app/app.js')}}"></script>
 @endsection
 
+@section('script')
+    <script type="text/javascript" src="{{ asset('js/jquery-ui.min.js') }}"></script>
+    <script>
+        $(function() {
+            // select menu
+            var select_menu = $('.select-menu');
+
+            if (select_menu.length > 0) {
+                select_menu.selectmenu();
+            }
+        });
+    </script>
+@endsection
+
 @section('style')
     <link rel="stylesheet" href="{{ mix('css/desktop/pages/user/mypage-lecture.css') }}">
 @endsection
