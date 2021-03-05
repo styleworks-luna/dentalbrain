dc3b2b1aeaa997a27f23e85e086c9217c71f7082
ndsrhkd <ndsrhkd@onoffmix.com>
2021-03-05 10:14:42 +0900

    [ENH] 슬랙 로깅 추가

--- config/logging.php
+++ config/logging.php
@@ -57,9 +57,9 @@ return [
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
+            'username' => 'DBV2020 LOG',
             'emoji' => ':boom:',
-            'level' => 'critical',
+            'level' => 'error',
         ],
 
         'papertrail' => [
