ec3ec995ccd2e03b36236e6aad184fb00ccd75c5
chdaud78 <qkrri56@naver.com>
2021-03-08 09:58:09 +0900

    [ENH] MICE-2017 추가 정보 입력 기본 틀 작성

--- resources/js/router/admin/lecture/online.js
+++ resources/js/router/admin/lecture/online.js
@@ -2,6 +2,7 @@ import Online from '@/views/admin/lecture/Online.vue';
 import OnlineCreate from '@/views/admin/lecture/OnlineCreate.vue';
 import OnlineEdit from '@/views/admin/lecture/OnlineEdit.vue';
 import OnlineStudent from '@/views/admin/lecture/OnlineStudent.vue';
+import OnlineAdditional from '@/views/admin/lecture/OnlineAdditional.vue';
 
 const routes = [
     {
@@ -23,6 +24,11 @@ const routes = [
         path: '/admin/lecture/online/:id',
         name: 'AdminOnlineEdit',
         component: OnlineEdit
+    },
+    {
+        path: '/admin/lecture/online/:id/student/additional',
+        name: 'AdminOnlineAdditional',
+        component: OnlineAdditional,
     },
 ];
 
