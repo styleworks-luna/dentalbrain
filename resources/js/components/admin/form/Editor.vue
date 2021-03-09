f4c2cb817330d9a74a48c9edba83d2fd09a46ccf
chdaud78 <qkrri56@naver.com>
2021-03-09 14:22:02 +0900

    [ADD] MICE-2041 CKEditor5 적용

--- resources/js/components/admin/form/Editor.vue
+++ resources/js/components/admin/form/Editor.vue
@@ -1,30 +1,49 @@
 <template>
-    <ckeditor :content="content" :config="editorConfig" @input="onEditorInput"></ckeditor>
+    <ckeditor :editor="editor" @ready="onReady" :content="content" :config="editorConfig" @input="onEditorInput"></ckeditor>
 </template>
 
 <script>
-import Vue from 'vue';
-import CKEditor from 'ckeditor4-vue';
-
-Vue.use(CKEditor);
+import CKEditor from '@ckeditor/ckeditor5-vue2';
+import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 
 export default {
     name: "Editor",
     props: {
         content: String,
     },
+    components: { ckeditor: CKEditor.component },
     data() {
         return {
+            editor: DecoupledEditor,
             editorConfig: {
-                extraPlugins: 'font, colorbutton, emoji, tableresize',
+                ckfinder: {
+                    uploadUrl: '/api/admin/lecture/upload' // 내가 지정한 업로드 url (post로 요청감)
+                }
             },
         }
     },
     methods: {
         onEditorInput(e) {
             this.$emit('setEditor', e)
-        }
+        },
+        onReady(editor) {
+            // Insert the toolbar before the editable area.
+            editor.ui.getEditableElement().parentElement.insertBefore(
+                editor.ui.view.toolbar.element,
+                editor.ui.getEditableElement()
+            );
+        },
     }
-
 };
 </script>
+
+<style scoped>
+.ck-editor__editable_inline {
+    height: 450px;
+    overflow-y: scroll;
+}
+
+.ck.ck-content.ck-editor__editable.ck-rounded-corners.ck-editor__editable_inline.ck-blurred {
+    border: 1px solid #000;
+}
+</style>
