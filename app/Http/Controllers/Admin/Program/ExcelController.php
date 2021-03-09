1e00f7cc17c0b7f8304ff5af8988a0fc30172c0a
ndsrhkd <ndsrhkd@onoffmix.com>
2021-03-09 14:05:08 +0900

    [DEL] MICE-2039 이전 코드 삭제

--- app/Http/Controllers/Admin/Program/ExcelController.php
+++ app/Http/Controllers/Admin/Program/ExcelController.php
@@ -13,6 +13,5 @@ class ExcelController extends Controller
     public function export(Request $request, Program $program)
     {
         return Excel::download(new StudentExport($program), '전체 명단 엑셀.xlsx');
-        //return Excel::download(new StudentExport($program), '전체 명단 엑셀.xlsx');
     }
 }
