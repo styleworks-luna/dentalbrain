b7bc50cf033d8eea7f07c4e3f8b786695104c40a
sunggun1 <sunggun1@onoffmix.com>
2021-03-10 14:57:37 +0900

    [ENH] MICE-2001 결제정보 검색

--- app/Http/Controllers/Admin/Payment/PaymentController.php
+++ app/Http/Controllers/Admin/Payment/PaymentController.php
@@ -9,23 +9,55 @@ use App\Models\Program\ProgramStudent;
 use App\Models\User;
 use App\Services\Program\OfflineProgramConcrete;
 use App\Services\Program\OnlineProgramConcrete;
+use App\Services\Search\SearchService;
+use Illuminate\Database\Connection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PaymentController extends Controller
 {
-    public function index()
+    private $search;
+
+    public function __construct()
+    {
+        $this->search = new SearchService(Payment::query());
+    }
+
+    public function index(Request $request)
     {
         $payments = Payment::query()
-            ->orderByDesc('id')
-            ->with(['student.ticket.program' => function ($query) {
-                $query->select('id', 'is_online', 'title');
-            }])
-            ->select('id', 'totalAmount', 'receiptUrl', 'method', 'status', 'requestedAt', 'approvedAt')
-            ->paginate(10);
+            ->select(
+                'payments.id','payments.totalAmount','payments.receiptUrl','payments.method','payments.status', 'payments.requestedAt','payments.approvedAt','payments.status',
+                'programs.is_online','programs.title',
+                'program_students.phone','program_students.email','program_students.user_id',
+                'program_tickets.program_id',
+                'users.name'
+                )
+            ->join('program_students','program_students.payment_id','=','payments.id')
+            ->join('program_tickets','program_students.ticket_id','=','program_tickets.id')
+            ->join('programs','programs.id','=','program_tickets.program_id')
+            ->join('users','users.id','=','program_students.user_id');
 
+        if(isset($request->is_online)){
+            $payments->where('programs.is_online','=',$request->is_online);
+        }
+
+        if(isset($request->status)){
+            $payments->where('payments.status','=',$request->status);
+        }
+
+        if(isset($request->keyword)){
+            $payments->where(function($query) use ($request){
+                $query->orWhere('programs.title','like','%'.$request->keyword.'%')
+                    ->orWhere('users.name','like','%'.$request->keyword.'%')
+                    ->orWhere('program_students.phone','like','%'.$request->keyword.'%')
+                    ->orWhere('program_students.email','like','%'.$request->keyword.'%');
+            });
+        }
+        
         return response()->json([
-            'payments' => $payments,
+            'payments' => $payments->paginate(10),
         ]);
     }
 
