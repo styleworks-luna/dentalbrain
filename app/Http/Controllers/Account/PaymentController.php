18c30a9ecbca9ecc9ea53ee424c4d5b62f2a39bd
sunggun1 <sunggun1@onoffmix.com>
2021-03-12 13:48:11 +0900

    [ENH] MICE-2001 mypage_payment 코드 변경

--- app/Http/Controllers/Account/PaymentController.php
+++ app/Http/Controllers/Account/PaymentController.php
@@ -12,7 +12,8 @@ class PaymentController extends Controller
     public function index()
     {
         $payments = Payment::query()
-            ->select('id', DB::raw("JSON_UNQUOTE(JSON_EXTRACT(full_response, '$.cancels[0]')) as cancel"), DB::raw("JSON_UNQUOTE(JSON_EXTRACT(full_response, '$.cancels[0].canceledAt')) as canceledAt"),'totalAmount', 'status', 'method', 'receiptUrl', 'requestedAt', 'va_accountNumber', 'va_bank', 'va_customerName', 'va_dueDate')
+            //->select('id', DB::raw("JSON_UNQUOTE(JSON_EXTRACT(full_response, '$.cancels[0]')) as cancel"), DB::raw("JSON_UNQUOTE(JSON_EXTRACT(full_response, '$.cancels[0].canceledAt')) as canceledAt"),'totalAmount', 'status', 'method', 'receiptUrl', 'requestedAt', 'va_accountNumber', 'va_bank', 'va_customerName', 'va_dueDate')
+            ->select('id', 'full_response' ,'totalAmount', 'status', 'method', 'receiptUrl', 'requestedAt', 'va_accountNumber', 'va_bank', 'va_customerName', 'va_dueDate')
             ->with(['student' => function ($query) {
                 $query->select('id', 'payment_id', 'ticket_id', 'user_id', 'expired_at');
                 $query->with(['ticket' => function ($query) {
@@ -23,6 +24,10 @@ class PaymentController extends Controller
             ->whereHas('student', function ($query) {
                 $query->where('user_id', Auth::id());
             })->get();
+        
+        foreach($payments as $payment){
+            $payment->full_response = json_decode($payment->full_response);
+        }
 
         return view(viewPrefix() . 'pages.user.mypage.mypage_payment', [
             'payments' => $payments,
