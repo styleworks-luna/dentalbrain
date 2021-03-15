4f7e776a4bc7d4984c069b1372ab655d85e34b12
chdaud78 <qkrri56@naver.com>
2021-03-15 15:31:00 +0900

    [ENH] MICE-2056 아이디 비밀번호 찾기 error 처리

--- public/js/pages/user/find.js
+++ public/js/pages/user/find.js
@@ -10,12 +10,16 @@ $(function() {
         messageWrap.html('');
 
         if (!name) {
-            alert('이름을 입력해주세요.');
+            $('.name').addClass('input-error');
+            $('.phone').removeClass('input-error');
+            $('.id-message').append('<p class="error">※ 이름을 입력해주세요.</p>');
             return false;
         }
 
         if (!phone) {
-            alert('휴대전화 번호를 입력해주세요.');
+            $('.phone').addClass('input-error');
+            $('.name').removeClass('input-error');
+            $('.id-message').append('<p class="error">※ 휴대전화 번호를 입력해주세요.</p>');
             return false;
         }
 
@@ -30,9 +34,13 @@ $(function() {
                 if (res.success) {
                     messageWrap.html('<p class="success">가입된 사용자 아이디는 <strong>' + res.login_id + '</strong> 입니다.</p>');
                     $('.find-id-form').css('height', '338px');
+                    $('.phone').removeClass('input-error');
+                    $('.name').removeClass('input-error');
                 } else {
                     messageWrap.html('<p class="error">' + '※ ' + res.message + '</p>');
                     $('.find-id-form').css('height', '322px');
+                    $('.phone').removeClass('input-error');
+                    $('.name').removeClass('input-error');
                 }
             },
             fail: function(err) {
@@ -45,13 +53,21 @@ $(function() {
     $('.btn-find-password').click(function(e) {
         var email = $('input[name="email"]').val();
         var messageWrap = $(this).closest('.find-form-common').find('.message-wrap');
+        var emailRule = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
         e.preventDefault();
 
         messageWrap.html('');
 
         if (!email) {
-            alert('이메일을 입력해주세요.');
+            $('.email').addClass('input-error');
+            $('.password-message').append('<p class="error">※ 이메일 주소를 입력해주세요.</p>');
+            return false;
+        }
+
+        if(email.match(emailRule) == null) {
+            $('.email').addClass('input-error');
+            $('.password-message').append('<p class="error">※ 이메일 형식에 맞춰 입력해주세요.</p>');
             return false;
         }
 
@@ -64,8 +80,10 @@ $(function() {
             success: function(res) {
                 if (res.success) {
                     messageWrap.html('<p class="send">' + res.message + '</p>');
+                    $('.email').removeClass('input-error');
                 } else {
-                    messageWrap.html('<p class="error">' + '※ ' + + res.message + '</p>');
+                    messageWrap.html('<p class="error">' + '※ ' + res.message + '</p>');
+                    $('.email').removeClass('input-error');
                 }
             },
             fail: function(err) {
--- resources/views/desktop/pages/user/find_id.blade.php
+++ resources/views/desktop/pages/user/find_id.blade.php
@@ -19,7 +19,7 @@
                 <input type="text" id="phone" name="phone" class="phone" placeholder="휴대전화 번호를 입력하세요. (숫자만 입력)">
                 <input type="button" class="btn-confirm btn-find-id" value="확인">
 
-                <div class="message-wrap"></div>
+                <div class="message-wrap id-message"></div>
             </div>
         </article>
 
@@ -31,7 +31,7 @@
                 <input type="text" id="email" name="email" class="email" placeholder="이메일을 입력하세요.">
                 <input type="button" class="btn-confirm btn-find-password" value="확인">
 
-                <div class="message-wrap"></div>
+                <div class="message-wrap password-message"></div>
             </div>
         </article>
     </section>
