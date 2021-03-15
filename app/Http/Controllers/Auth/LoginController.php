b2889c2b930fe01de2cffd9350b3e6df9b042b58
ndsrhkd <ndsrhkd@onoffmix.com>
2021-03-15 10:02:10 +0900

    [ENH] MICE-2059 비밀번호 미입력 얼럿 생성

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
@@ -45,22 +46,36 @@ class LoginController extends Controller
         return view(viewPrefix() . 'pages.user.login');
     }
 
-    public function username()
-    {
-        return 'login_id';
-    }
-
     public function authenticated(Request $request, $user)
     {
         $user->last_login_at = now();
         $user->save();
     }
 
+    /**
+     * Validate the user login request.
+     *
+     * @param Request $request
+     * @return void
+     *
+     * @throws ValidationException
+     */
     protected function validateLogin(Request $request)
     {
-        $request->validate([
+        $v = Validator::make($request->all(), [
             $this->username() => 'bail|required|string',
             'password' => 'required|string',
         ]);
+
+        if ($v->errors()->has('password')) {
+            session()->flash('alert', '비밀번호를 입력해주세요.');
+        }
+
+        $v->validate();
+    }
+
+    public function username()
+    {
+        return 'login_id';
     }
 }
