a645572d743c15b6ad7678aafd6f36293d0f58dd
sunggun1 <sunggun1@onoffmix.com>
2021-03-16 15:29:19 +0900

    [ENH] MICE-2087 공지사항 상세 진입 시 상단메뉴 컬러 및 하단바 누락

--- resources/views/desktop/pages/service/notice_detail.blade.php
+++ resources/views/desktop/pages/service/notice_detail.blade.php
@@ -8,7 +8,15 @@
     <section id="content">
         <div class="notice-detail-wrap">
             <div class="container">
-                @include('desktop.layouts.navigation.service')
+                <div class="service-menu">
+                    <h1>고객센터</h1>
+                    <ul>
+                        <li class="{{ (strpos(Route::currentRouteName(), 'customer.notices.show') !== false) ? 'active-menu' : '' }}"><a href="{{ route('customer.notices.index') }}">공지사항</a></li>
+                        <li class="{{ (strpos(Route::currentRouteName(), 'customer.faqs.index') !== false) ? 'active-menu' : '' }}"><a href="{{ route('customer.faqs.index') }}">FAQ</a></li>
+                        <li class="{{ (strpos(Route::currentRouteName(), 'customer.inquiries.index') !== false) ? 'active-menu' : '' }}"><a href="{{ route('customer.inquiries.index') }}">문의하기</a></li>
+                    </ul>
+                </div>
+
 
                 <section class="notice-detail">
                     <h2>공지사항</h2>
