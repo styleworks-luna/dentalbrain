de977c0809d6d9dde282522e0742d462b32e819a
chdaud78 <qkrri56@naver.com>
2021-03-23 13:25:42 +0900

    [ENH] MICE-2110 css 수정

--- resources/views/desktop/pages/introduce/community.blade.php
+++ resources/views/desktop/pages/introduce/community.blade.php
@@ -17,14 +17,34 @@
             </div>
         </div>
         <div class="container">
-            <div class="article-description">
-                @forelse($articles as $article)
-                    <img src="{{ $article->thumbnail->url }}" alt="{{ $article->thumbnail->name }}">
-                    <h2>{{ $article->title }}</h2>
-                    <p>{{ $article->date }}</p>
-                @empty
-                    <h1>없습니다</h1>
-                @endforelse
+            <div class="community-description">
+                <h2>관련 뉴스</h2>
+                <ul>
+                    <li>
+                        <div class="image-wrap">
+                            <img src="{{ url('images/dummy/test.png') }}" alt="">
+                        </div>
+                        <div class="community-information">
+                            <h3>코로나19로 인해 치과임상공부 집에서도 편하게하는 온라인라이브세미나를 시작했습니다. 코로나19로 인해 치과임상공부 집에서도 편하게하는
+                                온라인라이브세미나를
+                                시작했습니다. </h3>
+                            <p class="date">2020.11.17</p>
+                        </div>
+                        <span class="arrow-right"></span>
+                    </li>
+
+                    @forelse($articles as $article)
+                        <li>
+                            <img src="{{ $article->thumbnail->url }}" alt="{{ $article->thumbnail->name }}">
+                            <h3>{{ $article->title }}</h3>
+                            <p>{{ $article->date }}</p>
+                        </li>
+                    @empty
+                        <li>
+                            <p>없습니다</p>
+                        </li>
+                    @endforelse
+                </ul>
             </div>
         </div>
     </section>
