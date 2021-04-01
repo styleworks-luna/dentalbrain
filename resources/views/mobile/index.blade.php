7dcfd61fd3611ed3ffb4311611caac980be601d8
chdaud78 <qkrri56@naver.com>
2021-04-01 14:10:31 +0900

    [ENH] MICE-2121 모바일 메인 페이지 작업 중

--- resources/views/mobile/index.blade.php
+++ resources/views/mobile/index.blade.php
@@ -1,2 +1,49 @@
 @extends('mobile.layouts.frames.basic_frame')
 
+@section('script')
+    <script type="text/javascript" src="{{ asset('js/swiper-bundle.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/index.js') }}"></script>
+@endsection
+
+@section('vue')
+    <script type="text/javascript" src="{{ asset('js/app/app.js')}}"></script>
+@endsection
+
+@section('style')
+    <link rel="stylesheet" href="{{ asset('css/swiper-bundle.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/mobile/index.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/desktop/pages/lecture/lecture-all.css') }}">
+@endsection
+
+@section('content')
+    <section class="content">
+        <div class="m-container">
+
+            <section class="top-banner">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        @forelse($slides as $slide)
+                            <div class="swiper-slide">
+                                <a href="{{ route('api.banners.redirect',$slide->id) }}">
+                                    <img src="{{ $slide->desktopFile->url }}" alt="최상단 슬라이드">
+                                </a>
+                            </div>
+                        @empty
+                            <div class="swiper-slide">
+                                <a href="">
+                                    <img src="{{ asset('images/dummy/test2.jpg') }}" alt="최상단 슬라이드">
+                                </a>
+                            </div>
+                        @endforelse
+                    </div>
+                    <div class="swiper-controller-wrap">
+                        <div class="swiper-pagination"></div>
+                    </div>
+                </div>
+            </section>
+
+            <lecture-all :is_pagination="false" :per_page="9"></lecture-all>
+
+        </div>
+    </section>
+@endsection
