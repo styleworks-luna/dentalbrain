10d5ddc0263ea2a5de16bb3f60521f2b9cb836e8
ndsrhkd <ndsrhkd@onoffmix.com>
2021-04-13 15:53:15 +0900

    [ENH] MICE-2110 커뮤니티 좋아요 수정 * 유저 구분

--- app/Models/Manage/ArticleLike.php
+++ app/Models/Manage/ArticleLike.php
@@ -2,9 +2,20 @@
 
 namespace App\Models\Manage;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class ArticleLike extends Model
 {
     protected $table = 'article_likes';
+
+    public function user()
+    {
+        $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function article()
+    {
+        $this->belongsTo(Article::class, 'article_id', 'id');
+    }
 }
