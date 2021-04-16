dfd667fdccddb5b7e6b69819357ad4097f3fd372
chdaud78 <qkrri56@naver.com>
2021-04-16 15:52:08 +0900

    [ENH] MICE-2121 footer 위치 고정

--- resources/views/mobile/layouts/frames/basic_frame.blade.php
+++ resources/views/mobile/layouts/frames/basic_frame.blade.php
@@ -1,10 +1,12 @@
 @extends('mobile.layouts.app')
 
 @section('frame')
+    <div class="wrap">
     @include('mobile.layouts.header')
     @include('mobile.layouts.navigation.aside')
     <main class="main">
         @yield('content')
     </main>
     @include('mobile.layouts.footer')
+    </div>
 @endsection
