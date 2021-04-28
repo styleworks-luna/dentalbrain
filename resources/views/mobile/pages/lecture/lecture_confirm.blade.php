ada147d5e8faf05bb4448d5baa2085a1b79b7804
chdaud78 <qkrri56@naver.com>
2021-04-28 16:05:47 +0900

    [ENH] MICE-2121 강의 시청확인 페이지

--- resources/views/mobile/pages/lecture/lecture_confirm.blade.php
+++ resources/views/mobile/pages/lecture/lecture_confirm.blade.php
@@ -9,7 +9,11 @@
 
 @section('title')
     <a href="" class="btn-back"></a>
-    <h1>강의 시청확인</h1>
+    <div class="logo-wrap">
+        <a href="{{ url('/') }}" class="ir_pm header-logo">
+            <img src="{{ asset('/images/mobile/global/logo.svg') }}" alt="덴탈브레인">
+        </a>
+    </div>
 @endsection
 
 @section('content')
@@ -17,32 +21,30 @@
         <section class="lecture-confirm">
             <div class="m-container">
                 <div class="confirm-layer">
-                    <h1>강의 시청 안내</h1>
+                    <div class="layer-title">
+                        <h1>강의 시청 안내</h1>
+                        <a href="" class="close"></a>
+                    </div>
                     <div class="confirm-text">
-                        <ul>
-                            <li>
-                                <div class="text-wrap">
-                                    <span class="check"></span>
-                                    <p class="text">“여러분들을 위해 만든 교육의 가치를 지켜주세요”</p>
-                                </div>
-                                <p class="tip"><em>저작권법 제136조</em>, 온・오프라인 등지에서 무단전재 및 재배포,<br>
-                                    영상녹화 캡쳐 등 영상물 재가공 시 <em>5년 이하의 징역 또는 5천만원의 벌금</em></p>
-                            </li>
-                            <li>
-                                <div class="text-wrap">
-                                    <span class="check"></span>
-                                    <p class="text">강의를 시청하시면 환불이 불가능 합니다</p>
-                                </div>
-                            </li>
-                        </ul>
+                        <div class="img-wrap">
+                            <img src="{{ asset('images/mobile/lecture/lecture_check.svg') }}" alt=""/>
+                        </div>
+                        <div class="text-wrap">
+                            <p class="text">“여러분들을 위해 만든 교육의 가치를 지켜주세요”</p>
+                            <p class="tip">
+                                <em>저작권법 제136조</em>, 온・오프라인 등지에서 무단전재 및<br>
+                                재배포, 영상녹화 캡쳐 등 영상물 재가공 시 <br>
+                                <em>5년 이하의 징역 또는 5천만원의 벌금</em></p>
+                            <p class="text-last">강의를 시청하시면 환불이 불가능 합니다</p>
+                        </div>
                     </div>
                     <div class="btn-wrap">
                         <form action="{{ route('lectures.check-watch',['program' => $program]) }}" method="POST"
                               enctype="multipart/form-data">
                             @method('PATCH')
                             @csrf
-                            <button class="btn-confirm">확인</button>
                             <a href="#" onClick="history.back()">취소</a>
+                            <button class="btn-confirm">확인</button>
                         </form>
                     </div>
                 </div>
