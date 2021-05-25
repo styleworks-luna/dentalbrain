99f9244deff9ba42333c8cdabcda9ec38f7b544e
chdaud78 <qkrri56@naver.com>
2021-05-25 09:42:32 +0900

    [ENH] MICE-2203 유료회원 결제 기본틀 생성

--- resources/views/desktop/layouts/header.blade.php
+++ resources/views/desktop/layouts/header.blade.php
@@ -23,6 +23,7 @@
                 <li><a href="{{ url('instructor') }}">강사소개</a></li>
                 <li><a href="{{ url('community') }}">커뮤니티</a></li>
                 <li><a href="{{ url('lectures') }}">전체강의</a></li>
+                <li><a href="{{ url('lectures') }}">유료회원</a></li>
             </ul>
         </div>
         <div class="header-right">
