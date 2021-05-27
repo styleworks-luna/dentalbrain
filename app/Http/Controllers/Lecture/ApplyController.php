105896a89624135a0d893077f83471030b8fa6a9
ndsrhkd <ndsrhkd@onoffmix.com>
2021-05-27 15:24:18 +0900

    [DEL] 필요없는 기능 주석 처리함

--- app/Http/Controllers/Lecture/ApplyController.php
+++ app/Http/Controllers/Lecture/ApplyController.php
@@ -25,18 +25,19 @@ class ApplyController extends Controller
             // 이미 신청 완료하여 결제프로세스까지 마친 경우
             return redirect()->route('lectures.result', $program->id);
         }
-        $appliedBefore = ProgramStudent::query()->where('user_id', '=', Auth::id())
-            ->where('program_id', '=', $program->id)
-            ->whereIn('pay_status', [ProgramStudent::$PAY_BEFORE, ProgramStudent::$PAY_REFUNDED])
-            ->exists();
 
-        if ($appliedBefore) {
-            // 이전에 신청 폼을 완료하였을 경우.
-            return redirect()->route('lectures.payment.form', $program->id)->with([
-                'fromApply' => true,
-                'alert' => "신청정보가 존재하여 결제 페이지로 이동합니다."
-            ]);
-        }
+//        $appliedBefore = ProgramStudent::query()->where('user_id', '=', Auth::id())
+//            ->where('program_id', '=', $program->id)
+//            ->whereIn('pay_status', [ProgramStudent::$PAY_BEFORE, ProgramStudent::$PAY_REFUNDED])
+//            ->exists();
+//
+//        if ($appliedBefore) {
+//            // 이전에 신청 폼을 완료하였을 경우.
+//            return redirect()->route('lectures.payment.form', $program->id)->with([
+//                'fromApply' => true,
+//                'alert' => "신청정보가 존재하여 결제 페이지로 이동합니다."
+//            ]);
+//        }
 
         if ($program->is_online == 1) {
             // 온라인 강의
