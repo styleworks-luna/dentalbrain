9e9b2aea4c8aeefd23665c4c59970ea340a69f69
ndsrhkd <ndsrhkd@onoffmix.com>
2021-06-04 14:47:00 +0900

    [ENH] MICE-2212 찜 강의 분류 추가

--- app/Http/Controllers/Account/LikeController.php
+++ app/Http/Controllers/Account/LikeController.php
@@ -19,22 +19,33 @@ class LikeController extends Controller
         /** @var User $user */
         $user = User::query()->find(Auth::id());
 
-        return response()->json([
-            'programs' => $user->likePrograms()
-                ->with([
-                    'thumbnail' => function ($query) {
-                        $query->select(['id', 'url', 'name']);
-                    }, 'place' => function ($query) {
-                        $query->select(['id', 'program_id', 'address', 'address_detail',
-                            'started_at', 'ended_at']);
-                    }])
-                ->where('is_open', '=', 1)
-                ->get([
-                    'programs.id', 'programs.description', 'programs.major_category_id',
-                    'programs.minor_category_id', 'programs.title',
-                    'programs.price', 'programs.running_time', 'programs.thumbnail_id',
-                    'programs.is_online', 'programs.is_free']),
-        ]);
+        $programs = $user->likePrograms()
+            ->with([
+                'thumbnail' => function ($query) {
+                    $query->select(['id', 'url', 'name']);
+                }, 'place' => function ($query) {
+                    $query->select(['id', 'program_id', 'address', 'address_detail',
+                        'started_at', 'ended_at']);
+                }])
+            ->where('is_open', '=', 1)
+            ->orderByDesc('programs.created_at');
 
+
+        if ($data['order'] == 'online') {
+            $programs = $programs->where('is_online', '=', 1);
+        } elseif ($data['order'] == 'offline') {
+            $programs = $programs->where('is_online', '=', 0);
+        }
+
+        $programs = $programs->get([
+            'programs.id', 'programs.description', 'programs.major_category_id',
+            'programs.minor_category_id', 'programs.title',
+            'programs.price', 'programs.running_time', 'programs.thumbnail_id',
+            'programs.is_online', 'programs.is_free']);
+
+
+        return response()->json([
+            'programs' => $programs
+        ]);
     }
 }
