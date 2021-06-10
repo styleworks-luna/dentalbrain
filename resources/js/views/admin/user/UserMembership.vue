bd3ca9d53dbf15a1c0a45a2c3827b0f2e9e6e7d6
chdaud78 <qkrri56@naver.com>
2021-06-10 15:43:58 +0900

    [ENH] MICE-2218 쿼리 변경

--- resources/js/views/admin/user/UserMembership.vue
+++ resources/js/views/admin/user/UserMembership.vue
@@ -1,7 +1,7 @@
 <template>
     <layout title="유료 회원정보 목록">
         <template v-slot:button>
-            <a :href="`/api/admin/payment/export?keyword=${keyword}&start_date=${startDate}&end_date=${endDate}&status=${orderStatus}&category=${order}`"
+            <a :href="`/api/admin/payment/export?keyword=${keyword}&is_paid=${member}&job_name_id=${job_name_id}&page=${page}`"
                class="btn btn-info" download>엑셀 다운로드</a>
         </template>
         <template v-slot:search>
