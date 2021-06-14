f0e15f25e83d0c19af982d2511092f1c9a1ce622
chdaud78 <qkrri56@naver.com>
2021-06-14 10:01:41 +0900

    [ENH] MICE-2230 공지사항 Detail 페이지 에디터 적용에 따른 css, HTML 수정

--- resources/views/mobile/pages/service/notice_detail.blade.php
+++ resources/views/mobile/pages/service/notice_detail.blade.php
@@ -25,7 +25,9 @@
                                 </div>
                             </div>
                             <div class="notice-detail-content">
-                                <p>{{ $notice -> content }}</p>
+                                <div class="fr-element fr-view">
+                                    <p>{!! $notice -> content !!}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
