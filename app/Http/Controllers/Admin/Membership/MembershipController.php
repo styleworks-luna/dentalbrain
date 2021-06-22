a08385b814b6806fd4d5b293d7d8feaad6dbc31f
ndsrhkd <ndsrhkd@onoffmix.com>
2021-06-22 15:01:35 +0900

    [ENH] 유료회원 index 정보 출력 쿼리 수정

--- app/Http/Controllers/Admin/Membership/MembershipController.php
+++ app/Http/Controllers/Admin/Membership/MembershipController.php
@@ -33,7 +33,7 @@ class MembershipController extends Controller
 
         $search = new SearchService(User::query()
             ->with(['memberships' => function ($query) {
-                $query->inUse()->limit(1)->with('payment:id,method');
+                $query->inUse()->first()->with('payment:id,method');
             }])
             ->whereHas('memberships', function ($query) {
                 $query->with('payment:id,method');
