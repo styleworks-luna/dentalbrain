1c86a981bd43e031ebf1a1c223416886e8ca3c24
chdaud78 <qkrri56@naver.com>
2021-07-01 11:10:30 +0900

    [ENH] 유료회원 결제 slide down 으로 변경

--- resources/views/desktop/pages/membership/membership.blade.php
+++ resources/views/desktop/pages/membership/membership.blade.php
@@ -1,6 +1,175 @@
 @extends('desktop.layouts.frames.basic_frame')
 
 @section('script')
+    <script src="https://js.tosspayments.com/v1"></script>
+    <script type="text/javascript" src="{{ asset('js/jquery-ui.min.js') }}"></script>
+    <script>
+        $(function () {
+            var days;
+            var price;
+
+            // 연간 이벤트
+            $('.btn-apply-yearly').click(function (e) {
+                e.preventDefault();
+
+                console.log(1);
+                $('.yearly-membership-hidden').slideDown();
+                $('.btn-apply-yearly').css('display', 'none');
+                $('.btn-pay-yearly').css('display', 'block');
+            });
+
+            $('.btn-pay-yearly').click(function (e) {
+                days = 365;
+                price = 99000;
+            });
+            // 월간 이벤트
+            $('.btn-apply-monthly').click(function (e) {
+                e.preventDefault();
+
+                $('.monthly-membership-hidden').slideDown();
+                $('.btn-apply-monthly').css('display', 'none');
+                $('.btn-pay-monthly').css('display', 'block');
+            });
+
+            $('.btn-pay-monthly').click(function (e) {
+                days = 30;
+                price = 29000;
+            });
+
+            var select_menu = $('.select-menu');
+            var select_menu_02 = $('.select-menu-02')
+            var clientKey = '{{ env('TOSS_PAYMENTS_CLIENT_KEY') }}';
+            var tossPayments = TossPayments(clientKey);
+            var message = getParameter('message');
+            var paymentmethod = $('.payment-method:checked').val();
+            var paymentmethod_02 = $('.payment-method-02:checked').val();
+
+            // 결제 실패시 오류 메세지 출력
+            paymentMessage(message);
+
+            // select menu
+            if (select_menu.length > 0) {
+                select_menu.selectmenu();
+            }
+            if (select_menu_02.length > 0) {
+                select_menu_02.selectmenu();
+            }
+
+            $('.payment-method').change(function () {
+                paymentmethod = $('.payment-method:checked').val();
+            });
+            $('.payment-method-02').change(function () {
+                paymentmethod_02 = $('.payment-method-02:checked').val();
+            });
+
+            $('.btn-submit').click(function (e) {
+                e.preventDefault();
+
+                var paymentMethodResult;
+                var cardCompany
+                var check = false;
+
+                e.target.classList.forEach( x => {
+                    if(x == 'btn-pay-yearly') {
+                        return check = true;
+                    } else if(x == 'btn-pay-monthly') {
+                        return check = false;
+                    }
+                })
+
+                if(check) {
+                    paymentMethodResult = paymentmethod;
+                    cardCompany = $('#credit-button .ui-selectmenu-text').text();
+                } else {
+                    paymentMethodResult = paymentmethod_02;
+                    cardCompany = $('#credit_02-button .ui-selectmenu-text').text();
+                }
+                if (paymentMethodResult == "계좌입금") {
+                    $('.dim').css('display', 'block');
+                    $('.payment-layer-wrapper .layer').css('display', 'block');
+                } else {
+                    var paymentObj;
+
+                    const amount = price; // 1
+                    const orderId = '{{ \Illuminate\Support\Str::random(3) . time() }}';
+                    const orderName = `유료회원 ${days}일권` // 2
+                    const customerName = '{{ auth()->user()->name }}';
+                    const successUrl = '{{ route('membership.paymentSuccess') }}' + `?days=${days}`; // 3
+                    const customerEmail = '{{ auth()->user()->email }}';
+                    const customerMobilePhone = '{{ auth()->user()->phone }}';
+
+                    if (paymentMethodResult === '가상계좌') {
+                        paymentObj = {
+                            amount: amount,
+                            orderId: orderId,
+                            orderName: orderName,
+                            customerName: customerName,
+                            successUrl: successUrl,
+                            failUrl: window.location.href,
+                            customerEmail: customerEmail,
+                            customerMobilePhone: customerMobilePhone,
+                        };
+                    } else if (paymentMethodResult === '카드') {
+                        var maxCardInstallmentPlan = (cardCompany === 'BC' ? 3 : 12);
+
+                        paymentObj = {
+                            amount: amount,
+                            orderId: orderId,
+                            orderName: orderName,
+                            customerName: customerName,
+                            successUrl: successUrl,
+                            failUrl: window.location.href,
+                            customerEmail: customerEmail,
+                            customerMobilePhone: customerMobilePhone,
+
+                            maxCardInstallmentPlan: maxCardInstallmentPlan,
+                            cardCompany: cardCompany,
+                        };
+                    } else if (paymentMethodResult === '계좌이체') {
+                        paymentObj = {
+                            amount: amount,
+                            orderId: orderId,
+                            orderName: orderName,
+                            customerName: customerName,
+                            successUrl: successUrl,
+                            failUrl: window.location.href,
+                            customerEmail: customerEmail,
+                            customerMobilePhone: customerMobilePhone,
+                        };
+                    }
+
+                    tossPayments.requestPayment(paymentMethodResult, paymentObj).catch(function (err) {
+                        alert('취소');
+                    });
+                }
+            });
+
+            // 계좌입금 pop-up
+            $('.payment-layer-wrapper .btn-confirm').click(function (e) {
+                const attrHref = '{{ route('membership.paymentAnother') }}' + `?days=${days}`;
+                $('#separate_form').attr("action", `${attrHref}`).submit();
+            });
+
+            $('.payment-layer-wrapper .btn-cancel').click(function (e) {
+                $('.dim').css('display', 'none');
+                $('.payment-layer-wrapper .layer').css('display', 'none');
+            })
+        });
+
+        function getParameter(param) {
+            var paramData = window.location.search.substr(1).split('&').filter(function (i) {
+                return i.split('=')[0] == param;
+            });
+
+            return paramData.length === 0 ? null : paramData[0].split('=')[1];
+        }
+
+        function paymentMessage(message) {
+            if (message) {
+                alert(decodeURI(message));
+            }
+        }
+    </script>
 @endsection
 
 @section('style')
@@ -71,18 +240,132 @@
                             <span class="price">99,000원/연</span>
                             <p class="price-tip">연 회비 99,000원을 결제하면 자동으로 유료회원이 되고, 결제일로부터<br>
                                 1년 동안 무료강의와 할인 된 강의를 자유롭게 수강하실 수 있습니다.</p>
-                            <a href="#" class="btn-apply">신청하기</a>
+                            <div class="membership-payment-method yearly-membership-hidden">
+                                <span class="border-line">결제방식</span>
+                                <div class="radio-wrap">
+                                    <input type="radio" id="card" name="payment-method"
+                                           class="payment-method" value="카드" checked>
+                                    <label for="card">신용카드</label>
+
+                                    <select name="payment-method" id="credit" class="select-menu">
+                                        <option value="신한">신한</option>
+                                        <option value="현대">현대</option>
+                                        <option value="삼성">삼성</option>
+                                        <option value="우리">우리</option>
+                                        <option value="BC">BC</option>
+                                        <option value="국민">국민</option>
+                                        <option value="롯데">롯데</option>
+                                        <option value="농협">농협</option>
+                                        <option value="하나">하나</option>
+                                        <option value="씨티">씨티</option>
+                                        <option value="카카오뱅크">카카오뱅크</option>
+                                        <option value="수협">수협</option>
+                                        <option value="전북">전북</option>
+                                        <option value="우체국">우체국</option>
+                                        <option value="새마을">새마을</option>
+                                        <option value="저축">저축</option>
+                                        <option value="제주">제주</option>
+                                        <option value="광주">광주</option>
+                                        <option value="신협">신협</option>
+                                        <option value="JCB">JCB</option>
+                                        <option value="유니온페이">유니온페이</option>
+                                        <option value="마스터">마스터</option>
+                                        <option value="비자">비자</option>
+                                        <option value="다이너스">다이너스</option>
+                                        <option value="디스커버">디스커버</option>
+                                    </select>
+                                </div>
+                                <div class="radio-wrap transfer-wrap">
+                                    <input type="radio" id="transfer" name="payment-method"
+                                           class="payment-method" value="계좌이체">
+                                    <label for="transfer">{{ changePaymentMethodName("계좌이체") }}</label>
+                                </div>
+                                <div class="radio-wrap separate-wrap">
+                                    <input type="radio" id="separate" name="payment-method"
+                                           class="payment-method" value="계좌입금">
+                                    <label for="separate"
+                                           class="transfer-label">계좌입금</label>
+                                    <p class="separate-tip">신한은행 140-010-094358 예금주 : ㈜브레인스펙병원교육개발원</p>
+                                </div>
+                                {{--<div class="radio-wrap">
+                                    <input type="radio" id="deposit" name="payment-method"
+                                           class="payment-method" value="가상계좌">
+                                    <label for="deposit">무통장입금(가상계좌)</label>
+                                </div>--}}
+                                </td>
+                            </div>
+                            <a href="#" class="btn-apply btn-apply-yearly">신청하기</a>
+                            <a href="#" class="btn-apply btn-submit btn-pay-yearly">결제하기</a>
                         </div>
                         <div class="membership-price-item">
                             <h3>유료회원 월 결제</h3>
                             <span class="price">29,000원/월</span>
                             <p class="price-tip">월 회비 29,000원을 결제하면 자동으로 월 회원이 되고, 결제일로부터<br>
                                 한달 동안 무료강의와 할인 된 강의를 자유롭게 수강할 수 있습니다.</p>
-                            <a href="#" class="btn-apply">신청하기</a>
+                            <div class="membership-payment-method monthly-membership-hidden">
+                                <span class="border-line">결제방식</span>
+                                <div class="radio-wrap">
+                                    <input type="radio" id="card_02" name="payment-method_02"
+                                           class="payment-method-02" value="카드" checked>
+                                    <label for="card_02">신용카드</label>
+
+                                    <select name="payment-method_02" id="credit_02" class="select-menu-02">
+                                        <option value="신한">신한</option>
+                                        <option value="현대">현대</option>
+                                        <option value="삼성">삼성</option>
+                                        <option value="우리">우리</option>
+                                        <option value="BC">BC</option>
+                                        <option value="국민">국민</option>
+                                        <option value="롯데">롯데</option>
+                                        <option value="농협">농협</option>
+                                        <option value="하나">하나</option>
+                                        <option value="씨티">씨티</option>
+                                        <option value="카카오뱅크">카카오뱅크</option>
+                                        <option value="수협">수협</option>
+                                        <option value="전북">전북</option>
+                                        <option value="우체국">우체국</option>
+                                        <option value="새마을">새마을</option>
+                                        <option value="저축">저축</option>
+                                        <option value="제주">제주</option>
+                                        <option value="광주">광주</option>
+                                        <option value="신협">신협</option>
+                                        <option value="JCB">JCB</option>
+                                        <option value="유니온페이">유니온페이</option>
+                                        <option value="마스터">마스터</option>
+                                        <option value="비자">비자</option>
+                                        <option value="다이너스">다이너스</option>
+                                        <option value="디스커버">디스커버</option>
+                                    </select>
+                                </div>
+                                <div class="radio-wrap transfer-wrap">
+                                    <input type="radio" id="transfer_02" name="payment-method_02"
+                                           class="payment-method-02" value="계좌이체">
+                                    <label for="transfer_02">{{ changePaymentMethodName("계좌이체") }}</label>
+                                </div>
+                                <div class="radio-wrap separate-wrap">
+                                    <input type="radio" id="separate_02" name="payment-method_02"
+                                           class="payment-method-02" value="계좌입금">
+                                    <label for="separate_02"
+                                           class="transfer-label">계좌입금</label>
+                                    <p class="separate-tip">신한은행 140-010-094358 예금주 : ㈜브레인스펙병원교육개발원</p>
+                                </div>
+                                {{--<div class="radio-wrap">
+                                    <input type="radio" id="deposit_02" name="payment-method_02"
+                                           class="payment-method-02" value="가상계좌">
+                                    <label for="deposit_02">무통장입금(가상계좌)</label>
+                                </div>--}}
+                                </td>
+                            </div>
+                            <a href="#" class="btn-apply btn-apply-monthly">신청하기</a>
+                            <a href="#" class="btn-apply btn-submit btn-pay-monthly">결제하기</a>
                         </div>
                     </div>
                 </div>
+                <form method="POST" id="separate_form">
+                    @csrf
+                </form>
             </div>
         </div>
+        @include('desktop.component.popup.payment.payment_pop')
     </section>
 @endsection
