a9a0562dd5f84ce7274324912f90578f99a96fae
ndsrhkd <ndsrhkd@onoffmix.com>
2021-07-07 14:48:06 +0900

    Revert "[ENH] 유료회원 결제내역에 추가"
    This reverts commit 472b57cdba892e30a3fa106fd6cd23a8bb4b8327.

--- resources/views/desktop/pages/user/mypage/mypage_payment.blade.php
+++ resources/views/desktop/pages/user/mypage/mypage_payment.blade.php
@@ -17,196 +17,98 @@
                 <h2>결제내역</h2>
                 <ul>
                     @forelse($payments as $payment)
-                        @isset ($payment->student)
-                            <li>
-                                <div class="lecture-information">
-                                    @if ($payment->student->program->is_online)
-                                        <span class="online">온라인</span>
-                                    @else
-                                        <span class="offline">오프라인</span>
-                                    @endif
+                        <li>
+                            <div class="lecture-information">
+                                @if ($payment->student->program->is_online)
+                                    <span class="online">온라인</span>
+                                @else
+                                    <span class="offline">오프라인</span>
+                                @endif
 
-                                    <h3 class="lecture-name">{{ $payment->student->program->title }}</h3>
-                                </div>
-                                <table class="payment-information">
-                                    <tr>
-                                        <th>결제금액</th>
-                                        <th>결제상태</th>
-                                        <th>결제수단</th>
-                                        <th>결제일{{ $payment->cancel != 'null' ? "/취소일" : '' }}</th>
-                                    </tr>
-                                    <tr>
-                                        <td>{{ number_format($payment->totalAmount) }}원</td>
-                                        @switch($payment->status)
-                                            @case('READY')
-                                            @case('IN_PROGRESS')
-                                            <td>진행 중</td>
-                                            @break
+                                <h3 class="lecture-name">{{ $payment->student->program->title }}</h3>
+                            </div>
+                            <table class="payment-information">
+                                <tr>
+                                    <th>결제금액</th>
+                                    <th>결제상태</th>
+                                    <th>결제수단</th>
+                                    <th>결제일{{ $payment->cancel != 'null' ? "/취소일" : '' }}</th>
+                                </tr>
+                                <tr>
+                                    <td>{{ number_format($payment->totalAmount) }}원</td>
+                                    @switch($payment->status)
+                                        @case('READY')
+                                        @case('IN_PROGRESS')
+                                        <td>진행 중</td>
+                                        @break
 
-                                            @case('WAITING_FOR_DEPOSIT')
-                                            <td>입금 대기중</td>
-                                            @break
+                                        @case('WAITING_FOR_DEPOSIT')
+                                        <td>입금 대기중</td>
+                                        @break
 
-                                            @case('DONE')
-                                            @case('ANOTHER_DONE')
-                                            <td>결제 완료</td>
-                                            @break
+                                        @case('DONE')
+                                        @case('ANOTHER_DONE')
+                                        <td>결제 완료</td>
+                                        @break
 
-                                            @case('ABORTED')
-                                            <td>결제 오류</td>
-                                            @break
+                                        @case('ABORTED')
+                                        <td>결제 오류</td>
+                                        @break
 
-                                            @case('CANCELED')
-                                            @case('PARTIAL_CANCELED')
-                                            <td class="cancel">결제 취소</td>
-                                            @break
+                                        @case('CANCELED')
+                                        @case('PARTIAL_CANCELED')
+                                        <td class="cancel">결제 취소</td>
+                                        @break
 
-                                            @case('ANOTHER_PROGRESS')
-                                            <td>입금 대기</td>
-                                            @break
+                                        @case('ANOTHER_PROGRESS')
+                                        <td>입금 대기</td>
+                                        @break
 
-                                            @case('ANOTHER_REJECTED')
-                                            <td>신청 취소</td>
-                                            @break
+                                        @case('ANOTHER_REJECTED')
+                                        <td>신청 취소</td>
+                                        @break
 
-                                            @default
-                                            <td>확인 중</td>
-                                        @endswitch
-                                        <td>
-                                            {{ changePaymentMethodName($payment->method) }}
-                                            @if($payment->method == '가상계좌' && $payment->status =='WAITING_FOR_DEPOSIT')
-                                                <a href="" class="waiting-deposit">자세히 보기</a>
-                                                <div class="deposit-detail">
-                                                    <p>입금 계좌 : {{ $payment->va_accountNumber }}</p>
-                                                    <p>예금주 : {{ $payment->va_customerName }}</p>
-                                                    <p>납입기한 : {{ date_format($payment->va_dueDate,'Y.m.d G:i:s') }}</p>
-                                                </div>
-                                            @elseif($payment->method == '계좌입금' && $payment->status == 'ANOTHER_PROGRESS')
-                                                <a href="" class="waiting-deposit">자세히 보기</a>
-                                                <div class="deposit-detail">
-                                                    <p>입금 계좌 : 신한은행 140-010-094358 </p>
-                                                    <p>예금주 : (주)브레인스펙병원교육개발원</p>
-                                                </div>
-                                            @elseif($payment->method == '계좌입금' && ($payment->status == 'DONE' || $payment->status == 'ANOTHER_DONE'))
-                                                <span class="tip">(영수증 관리자 문의)</span>
-                                            @endif
-                                            @isset($payment->receiptUrl)
-                                                @if ($payment->status == 'DONE')
-                                                    <a href="{{ $payment->receiptUrl }}" target="_blank">결제 영수증</a>
-                                                @elseif($payment->status == 'CANCELED')
-                                                    <a href="{{ $payment->receiptUrl }}" target="_blank">취소 영수증</a>
-                                                @endif
-                                            @endisset
-                                        </td>
-                                        <td>
-                                            <div
-                                                class="@isset($payment->full_response->cancels) payment-cancel @endisset">
-                                                {{ date_format($payment->requestedAt ,'Y.m.d')}}
+                                        @default
+                                        <td>확인 중</td>
+                                    @endswitch
+                                    <td>
+                                        {{ changePaymentMethodName($payment->method) }}
+                                        @if($payment->method == '가상계좌' && $payment->status =='WAITING_FOR_DEPOSIT')
+                                            <a href="" class="waiting-deposit">자세히 보기</a>
+                                            <div class="deposit-detail">
+                                                <p>입금 계좌 : {{ $payment->va_accountNumber }}</p>
+                                                <p>예금주 : {{ $payment->va_customerName }}</p>
+                                                <p>납입기한 : {{ date_format($payment->va_dueDate,'Y.m.d G:i:s') }}</p>
                                             </div>
-
-                                            @isset($payment->full_response->cancels)
-                                                {{'/ ' . date('Y.m.d',strtotime($payment->full_response->cancels[0]->canceledAt))}}
-                                            @endisset
-                                        </td>
-                                    </tr>
-                                </table>
-                            </li>
-                        @endisset
-                        @isset($payment->membership)
-                            <li>
-                                <div class="lecture-information">
-                                    @if ($payment->student->program->is_online)
-                                        <span class="online">온라인</span>
-                                    @else
-                                        <span class="offline">오프라인</span>
-                                    @endif
-
-                                    <h3 class="lecture-name">{{ $payment->student->program->title }}</h3>
-                                </div>
-                                <table class="payment-information">
-                                    <tr>
-                                        <th>결제금액</th>
-                                        <th>결제상태</th>
-                                        <th>결제수단</th>
-                                        <th>결제일{{ $payment->cancel != 'null' ? "/취소일" : '' }}</th>
-                                    </tr>
-                                    <tr>
-                                        <td>{{ number_format($payment->totalAmount) }}원</td>
-                                        @switch($payment->status)
-                                            @case('READY')
-                                            @case('IN_PROGRESS')
-                                            <td>진행 중</td>
-                                            @break
-
-                                            @case('WAITING_FOR_DEPOSIT')
-                                            <td>입금 대기중</td>
-                                            @break
-
-                                            @case('DONE')
-                                            @case('ANOTHER_DONE')
-                                            <td>결제 완료</td>
-                                            @break
-
-                                            @case('ABORTED')
-                                            <td>결제 오류</td>
-                                            @break
-
-                                            @case('CANCELED')
-                                            @case('PARTIAL_CANCELED')
-                                            <td class="cancel">결제 취소</td>
-                                            @break
-
-                                            @case('ANOTHER_PROGRESS')
-                                            <td>입금 대기</td>
-                                            @break
-
-                                            @case('ANOTHER_REJECTED')
-                                            <td>신청 취소</td>
-                                            @break
-
-                                            @default
-                                            <td>확인 중</td>
-                                        @endswitch
-                                        <td>
-                                            {{ changePaymentMethodName($payment->method) }}
-                                            @if($payment->method == '가상계좌' && $payment->status =='WAITING_FOR_DEPOSIT')
-                                                <a href="" class="waiting-deposit">자세히 보기</a>
-                                                <div class="deposit-detail">
-                                                    <p>입금 계좌 : {{ $payment->va_accountNumber }}</p>
-                                                    <p>예금주 : {{ $payment->va_customerName }}</p>
-                                                    <p>납입기한 : {{ date_format($payment->va_dueDate,'Y.m.d G:i:s') }}</p>
-                                                </div>
-                                            @elseif($payment->method == '계좌입금' && $payment->status == 'ANOTHER_PROGRESS')
-                                                <a href="" class="waiting-deposit">자세히 보기</a>
-                                                <div class="deposit-detail">
-                                                    <p>입금 계좌 : 신한은행 140-010-094358 </p>
-                                                    <p>예금주 : (주)브레인스펙병원교육개발원</p>
-                                                </div>
-                                            @elseif($payment->method == '계좌입금' && ($payment->status == 'DONE' || $payment->status == 'ANOTHER_DONE'))
-                                                <span class="tip">(영수증 관리자 문의)</span>
-                                            @endif
-                                            @isset($payment->receiptUrl)
-                                                @if ($payment->status == 'DONE')
-                                                    <a href="{{ $payment->receiptUrl }}" target="_blank">결제 영수증</a>
-                                                @elseif($payment->status == 'CANCELED')
-                                                    <a href="{{ $payment->receiptUrl }}" target="_blank">취소 영수증</a>
-                                                @endif
-                                            @endisset
-                                        </td>
-                                        <td>
-                                            <div
-                                                class="@isset($payment->full_response->cancels) payment-cancel @endisset">
-                                                {{ date_format($payment->requestedAt ,'Y.m.d')}}
+                                        @elseif($payment->method == '계좌입금' && $payment->status == 'ANOTHER_PROGRESS')
+                                            <a href="" class="waiting-deposit">자세히 보기</a>
+                                            <div class="deposit-detail">
+                                                <p>입금 계좌 : 신한은행 140-010-094358 </p>
+                                                <p>예금주 : (주)브레인스펙병원교육개발원</p>
                                             </div>
+                                        @elseif($payment->method == '계좌입금' && ($payment->status == 'DONE' || $payment->status == 'ANOTHER_DONE'))
+                                            <span class="tip">(영수증 관리자 문의)</span>
+                                        @endif
+                                        @isset($payment->receiptUrl)
+                                            @if ($payment->status == 'DONE')
+                                                <a href="{{ $payment->receiptUrl }}" target="_blank">결제 영수증</a>
+                                            @elseif($payment->status == 'CANCELED')
+                                                <a href="{{ $payment->receiptUrl }}" target="_blank">취소 영수증</a>
+                                            @endif
+                                        @endisset
+                                    </td>
+                                    <td>
+                                        <div class="@isset($payment->full_response->cancels) payment-cancel @endisset">
+                                            {{ date_format($payment->requestedAt ,'Y.m.d')}}
+                                        </div>
 
-                                            @isset($payment->full_response->cancels)
-                                                {{'/ ' . date('Y.m.d',strtotime($payment->full_response->cancels[0]->canceledAt))}}
-                                            @endisset
-                                        </td>
-                                    </tr>
-                                </table>
-                            </li>
-                        @endisset
+                                        @isset($payment->full_response->cancels)
+                                            {{'/ ' . date('Y.m.d',strtotime($payment->full_response->cancels[0]->canceledAt))}}
+                                        @endisset
+                                    </td>
+                                </tr>
+                            </table>
+                        </li>
                     @empty
                         <li class="payment-none">결제내역이 없습니다.</li>
                     @endforelse
