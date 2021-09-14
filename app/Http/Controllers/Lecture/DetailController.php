bc2ec9efcce4e3d5773d2a3550c38d79be9acee2
ndsrhkd <ndsrhkd@onoffmix.com>
2021-09-14 16:06:48 +0900

    [ENH] 비공개 강의 비신청자 못들어가게끔 수정

--- app/Http/Controllers/Lecture/DetailController.php
+++ app/Http/Controllers/Lecture/DetailController.php
@@ -18,6 +18,10 @@ class DetailController extends Controller
 
     public function detail(Program $program)
     {
+        if (!$this->availableViewLecture($program)) {
+            return redirect()->back()->with("alert", "비공개된 강의입니다.");
+        }
+
         $heart = UserLike::query()->where('program_id', '=', $program->id)
             ->where('user_id', '=', Auth::id())
             ->exists();
@@ -46,6 +50,29 @@ class DetailController extends Controller
             'children' => $children,
             'student' => $student,
         ]);
+    }
+
+    private function availableViewLecture(Program $program): bool
+    {
+        if ($program->is_open) {
+            return true;
+        }
+
+        // 비공개
+
+        if (!Auth::check()) {
+            return false;
+        }
+
+        // 로그인
+
+        if (Auth::user()->is_admin || $program->alreadyApplied()) {
+            // 어드민
+            return true;
+        }
+
+        // 볼 수 없는 사용자.
+        return false;
     }
 
     public function like(Request $request, Program $program)
