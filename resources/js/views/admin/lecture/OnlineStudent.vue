a1b68c06ff61514b258f347fb80b3e7201a46849
chdaud78 <qkrri56@naver.com>
2022-02-03 16:12:25 +0900

    [ENH] user 상세 membership으로 변경

--- resources/js/views/admin/lecture/OnlineStudent.vue
+++ resources/js/views/admin/lecture/OnlineStudent.vue
@@ -34,7 +34,7 @@
                     <td>{{ slotProps.row.student_id }}</td>
                     <td>{{ slotProps.row.is_paid ? '유료회원' : '일반' }}</td>
                     <td>
-                        <router-link :to="`/admin/user/user/${slotProps.row.user_id}/1`">
+                        <router-link :to="`/admin/user/membership/${slotProps.row.user_id}/1`">
                             {{ slotProps.row.login_id }}
                         </router-link>
                     </td>
