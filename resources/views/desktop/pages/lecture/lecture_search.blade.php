fedfa67970331697e3e153c0bebfc296a6ae01aa
gusrns48 <gusrns48@naver.com>
2022-02-15 17:17:42 +0900

    [ENH] 검색어 페이지 수정

--- resources/views/desktop/pages/lecture/lecture_search.blade.php
+++ resources/views/desktop/pages/lecture/lecture_search.blade.php
@@ -1,31 +1,32 @@
 @extends('desktop.layouts.frames.basic_frame')
 
 @section('vue')
-    <script type="text/javascript" src="{{ asset('js/app/app.js')}}"></script>
+<script type="text/javascript" src="{{ asset('js/app/app.js')}}"></script>
 @endsection
 
 @section('style')
-    <link rel="stylesheet" href="{{ mix('css/desktop/pages/lecture/lecture-all.css') }}">
+<link rel="stylesheet" href="{{ mix('css/desktop/pages/lecture/lecture-all.css') }}">
+<link rel="stylesheet" href="{{ mix('css/desktop/pages/lecture/lecture-list.css') }}">
 @endsection
 
 @section('content')
-    <div id="content">
-        <section class="search">
-            <article class="lecture-title-wrap">
-                <h1>검색 결과</h1>
+<div id="content">
+    <section class="search">
+        <article class="lecture-title-wrap">
+            <h1>검색 결과</h1>
+        </article>
+        <div class="container">
+            <article class="search-text-wrap">
+
+                <span class="search-text"></span>
+                <span>&nbsp;검색결과</span>
             </article>
-            <div class="container">
-                <article class="search-text-wrap">
 
-                    <span class="search-text"></span>
-                    <span>&nbsp;검색결과</span>
-                </article>
+            <article class="lecture-wrap">
+                <lecture-all :is_pagination="true" :per_page="12"></lecture-all>
+            </article>
+        </div>
 
-                <article class="lecture-wrap">
-                    <lecture-all :is_pagination="true" :per_page="12"></lecture-all>
-                </article>
-            </div>
-
-        </section>
-    </div>
+    </section>
+</div>
 @endsection
