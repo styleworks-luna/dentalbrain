7d0435e8ccff4f6304d3328a2b8af8dd7322d4ba
gusrns48 <gusrns48@naver.com>
2022-03-16 09:46:48 +0900

    [ENH] 모바일 강의 카테고리 수정

--- resources/views/mobile/pages/lecture/lecture_apply.blade.php
+++ resources/views/mobile/pages/lecture/lecture_apply.blade.php
@@ -29,15 +29,9 @@
                     <section class="lecture-information-wrap">
                         <div class="lecture-information">
                             <div class="lecture-sort">
-                                @if($program->is_online == true)
-                                    <span class="online">온라인</span>
-                                @else
-                                    <span class="offline">오프라인</span>
-                                @endif
+                                <span class="lecture-type">{{$program->minor_category_name}}</span>
 
-                                <p class="lecture-subject">
-                                    {{ $program->major_category_name }} @isset($program->minor_category_name) &middot; {{ $program->minor_category_name}} @endisset
-                                </p>
+                                <p class="lecture-date">수강기간 10일</p>
                                 @if($program->is_online == true)
                                     <p class="lecture-length">{{ $program->running_time }}</p>
                                 @endif
