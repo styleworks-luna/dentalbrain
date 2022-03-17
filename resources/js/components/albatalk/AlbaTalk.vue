c1831730e37f056a3058a59cfcd2824a95b96e52
gusrns48 <gusrns48@naver.com>
2022-03-17 10:33:13 +0900

    [ENH] 메인 네비게이션 디자인

--- resources/js/components/albatalk/AlbaTalk.vue
+++ resources/js/components/albatalk/AlbaTalk.vue
@@ -1,16 +1,19 @@
 <template>
     <section class="albatalk">
+        <albatalk-navigation></albatalk-navigation>
         <albatalk-list></albatalk-list>
     </section>
 </template>
 
 <script>
 import AlbaTalkList from '@/components/albatalk/AlbaTalkList.vue';
+import AlbaTalkNavigation from '@/components/albatalk/AlbaTalkNavigation.vue';
 
 export default {
     name: "AlbaTalk",
     components: {
         'albatalk-list': AlbaTalkList,
+        'albatalk-navigation': AlbaTalkNavigation,
     },
 }
 </script>
