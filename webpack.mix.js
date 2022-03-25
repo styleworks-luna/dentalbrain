2370c0df7fa7fc3cd283b232cd38e118fed5ee6d
gusrns48 <gusrns48@naver.com>
2022-03-25 17:23:36 +0900

    [ENH] 알바톡 이력서 확인페이지 디자인

--- webpack.mix.js
+++ webpack.mix.js
@@ -64,6 +64,7 @@ mix.sass('resources/sass/desktop/pages/membership/membership.sass', 'public/css/
 mix.sass('resources/sass/desktop/pages/albatalk/albatalk-all.sass', 'public/css/desktop/pages/albatalk');
 mix.sass('resources/sass/desktop/pages/albatalk/albatalk-post.sass', 'public/css/desktop/pages/albatalk');
 mix.sass('resources/sass/desktop/pages/albatalk/albatalk-detail.sass', 'public/css/desktop/pages/albatalk');
+mix.sass('resources/sass/desktop/pages/albatalk/albatalk-resume.sass', 'public/css/desktop/pages/albatalk');
 
 // 강의 페이지
 mix.sass('resources/sass/desktop/pages/lecture/lecture-detail.sass', 'public/css/desktop/pages/lecture')
