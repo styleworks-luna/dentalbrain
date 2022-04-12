95644126b9961c445624b81861caa810b27b0d53
chdaud78 <qkrri56@naver.com>
2022-04-12 11:39:17 +0900

    [ENH] 알바톡 헤드헌팅 페이지 및 구인등록 결제 금액 페이지 layout 작업

--- resources/js/views/admin/albatalk/AlbatalkHeadHunting.vue
+++ resources/js/views/admin/albatalk/AlbatalkHeadHunting.vue
@@ -1,6 +1,15 @@
 <template>
-    <layout title="헤드 헌팅">
+    <layout title="헤드 헌팅 주소 입력">
         <template v-slot:body>
+            <div class="input-group w-50 mb-5">
+                <input class="form-control"
+                       type="text"
+                       placeholder="https:// 로 시작하는 url 입력"
+                       v-model="url">
+                <span class="input-group-append">
+                    <button class="btn btn-primary" type="submit" @click.prevent="edit">수정</button>
+                </span>
+            </div>
         </template>
     </layout>
 </template>
@@ -8,5 +17,15 @@
 <script>
 export default {
     name: "AlbaTalkHeadHunting",
+    data() {
+        return {
+            url: "",
+        }
+    },
+    methods: {
+        edit() {
+
+        }
+    }
 }
 </script>
--- resources/js/views/admin/albatalk/AlbatalkRecruitPayment.vue
+++ resources/js/views/admin/albatalk/AlbatalkRecruitPayment.vue
@@ -1,6 +1,32 @@
 <template>
-    <layout title="구인등록 결제 관리">
+    <layout title="구인등록 결제 금액">
         <template v-slot:body>
+            <div class="input-group w-50 mb-5">
+                <div class="input-group-prepend">
+                    <label for="price" class="input-group-text">일반회원가</label>
+                </div>
+                <input class="form-control"
+                       type="text"
+                       id="price"
+                       placeholder="일반 회원가 입력"
+                       v-model="price">
+                <span class="input-group-append">
+                    <button class="btn btn-primary" type="submit" @click.prevent="changePrice">적용</button>
+                </span>
+            </div>
+            <div class="input-group w-50 mb-5">
+                <div class="input-group-prepend">
+                    <label for="membership_price" class="input-group-text">유료회원가</label>
+                </div>
+                <input class="form-control"
+                       type="text"
+                       id="membership_price"
+                       placeholder="유료 회원가 입력"
+                       v-model="membership_price">
+                <span class="input-group-append">
+                    <button class="btn btn-primary" type="submit" @click.prevent="changeMembershipPrice">수정</button>
+                </span>
+            </div>
         </template>
     </layout>
 </template>
@@ -8,5 +34,19 @@
 <script>
 export default {
     name: "AlbaTalkRecruitPayment",
+    data() {
+        return {
+            price: "",
+            membership_price: "",
+        }
+    },
+    methods: {
+        changePrice() {
+
+        },
+        changeMembershipPrice() {
+
+        }
+    }
 }
 </script>
