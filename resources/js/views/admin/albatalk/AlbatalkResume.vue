e8d057f147d60457bbc70a20e2177a2f41a81fe5
chdaud78 <qkrri56@naver.com>
2022-04-12 11:26:26 +0900

    [ENH] 알바톡 관리자 구직 정보 페이지 작업

--- resources/js/views/admin/albatalk/AlbatalkResume.vue
+++ resources/js/views/admin/albatalk/AlbatalkResume.vue
@@ -1,12 +1,121 @@
 <template>
-    <layout title="구직정보">
+    <layout title="구직 정보">
+        <template v-slot:search>
+            <span style="font-size: 12px">구직 정보 [ 전체 0개 ]</span>
+            <div class="float-right">
+                <form @submit.prevent="">
+                    <div class="input-group">
+                        <input class="form-control"
+                               type="text"
+                               placeholder="제목"
+                               v-model="keyword">
+                        <span class="input-group-append">
+                            <button class="btn btn-primary" type="submit">검색</button>
+                        </span>
+                    </div>
+                </form>
+            </div>
+        </template>
+
         <template v-slot:body>
+            <table-grid :tableCol="tableCol"
+                        :data="resumeList">
+                <template v-slot:list="slotProps">
+                    <td>{{ slotProps.row.id }}</td>
+                    <td>
+                        <router-link :to="`/admin/user/user/${slotProps.row.user_id}/1`">
+                            {{ slotProps.row.user_id }}
+                        </router-link>
+                    </td>
+                    <td><router-link :to="`/admin/user/user/${slotProps.row.user_id}/1`">{{ slotProps.row.name }}</router-link></td>
+                    <td><router-link :to="`/admin/user/user/${slotProps.row.user_id}/1`">{{ slotProps.row.email }}</router-link></td>
+                    <td>{{ slotProps.row.email }}</td>
+                    <td>{{ slotProps.row.job }}</td>
+                    <td>
+                        <a :href="`/albatalk/resume/${slotProps.row.id}`"
+                           class="btn btn-info float-left mr-2">
+                            보기
+                        </a>
+                    </td>
+                    <td>
+                        <button class="btn btn-danger float-left" @click="">추천</button>
+                    </td>
+                </template>
+            </table-grid>
+
+            <!--<div class="paging-wrap text-center">
+                <nav class="d-inline-block">
+                    <pagination :data="resumeList" :limit=3 @pagination-change-page="" class="mb-0">
+                        <span slot="prev-nav">‹</span>
+                        <span slot="next-nav">›</span>
+                    </pagination>
+                </nav>
+            </div>-->
         </template>
     </layout>
 </template>
 
 <script>
+import Table from '@/components/admin/grid/Table.vue';
+
 export default {
     name: "AlbaTalkResume",
+    components: {
+        'table-grid': Table,
+    },
+    computed: {
+        tableCol() {
+            return [
+                {
+                    name: 'id',
+                    text: '번호',
+                    width: '5%'
+                },
+                {
+                    name: 'user_id',
+                    text: '아이디',
+                    width: '12%'
+                },
+                {
+                    name: 'name',
+                    text: '이름',
+                    width: '12%'
+                },
+                {
+                    name: 'email',
+                    text: '이메일',
+                    width: '18%'
+                },
+                {
+                    name: 'phone',
+                    text: '전화번호',
+                    width: '18%'
+                },
+                {
+                    name: 'job',
+                    text: '직업군',
+                    width: '15%'
+                },
+                {
+                    name: 'status',
+                    text: '이력서',
+                    width: '10%'
+                },
+                {
+                    name: 'function',
+                    text: '추천하기',
+                    width: '10%'
+                },
+            ]
+        },
+    },
+    data() {
+        return {
+            resumeList: [],
+            keyword: "",
+        }
+    },
+    methods: {
+    }
 }
 </script>
