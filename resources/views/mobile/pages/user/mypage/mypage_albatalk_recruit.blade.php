b38de96f693a3e6ef22a7e8a357252aff6749f2e
gusrns48 <gusrns48@naver.com>
2022-04-13 13:20:32 +0900

    [ENH] 마이페이지 설정

---
+++ resources/views/mobile/pages/user/mypage/mypage_albatalk_recruit.blade.php
@@ -0,0 +1,28 @@
+@extends('mobile.layouts.frames.except_frame')
+
+@section('vue')
+    <script type="text/javascript" src="{{ asset('js/app/app.js')}}"></script>
+@endsection
+
+@section('style')
+    <link rel="stylesheet" href="{{ mix('css/mobile/pages/user/mypage/mypage-albatalk-recruit.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/mobile/pages/albatalk/albatalk-list.css') }}">
+@endsection
+
+@section('title')
+    <div class="menu-btn-wrap">
+        <a href="" class="menu-btn"></a>
+    </div>
+    <a href="" class="btn-back"></a>
+    <h1>구인정보</h1>
+@endsection
+
+@section('content')
+    <section class="albatalk-wrap">
+        <div class="m-container">
+            <div class="m-row">
+                <albatalk></albatalk>
+            </div>
+        </div>
+    </section>
+@endsection
