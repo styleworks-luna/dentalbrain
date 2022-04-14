fca014c71076811059f432cb04a66953300559ff
ndsrhkd <ndsrhkd@onoffmix.com>
2022-04-14 13:43:01 +0900

    [ADD] import 추가

--- app/Http/Controllers/Albatalk/Recruit/RecruitController.php
+++ app/Http/Controllers/Albatalk/Recruit/RecruitController.php
@@ -24,6 +24,7 @@ use App\Services\Recruit\RecruitService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class RecruitController extends Controller
 {
