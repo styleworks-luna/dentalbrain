ee48e20cb62b602e5f60a6693db6ef95e3505bcb
ndsrhkd <ndsrhkd@onoffmix.com>
2022-04-14 13:38:32 +0900

    [ENH] 검색 param 변경

--- app/Http/Controllers/Albatalk/Recruit/RecruitSearchController.php
+++ app/Http/Controllers/Albatalk/Recruit/RecruitSearchController.php
@@ -6,18 +6,25 @@ use App\Http\Controllers\Controller;
 use App\Models\Recruit\Recruit;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Pagination\Paginator;
 use Illuminate\Validation\Rule;
 
 class RecruitSearchController extends Controller
 {
     public function search(Request $request): JsonResponse
     {
+        $isAcceptableSido = function ($attribute, $value, $fail) {
+            $params = collect(explode(',', $value));
+            $matched = $params->intersect(RecruitSiDo::getArray())->count() == $params->count();
+            if (!$matched) {
+                $fail('잘못된 입력값입니다.');
+            }
+        };
+
         $validated = $request->validate([
-            'sido' => ['required', Rule::in(array_merge(RecruitSiDo::getArray(), ['all'])),],
+            'sido' => ['required', $isAcceptableSido,],
             'order' => ['nullable', Rule::in(['newest', 'closest'])],
         ]);
-        $sido = $validated['sido'];
+        $sido = explode(',', $validated['sido']);
         $order = $validated['order'] ?? 'newest';
 
         $builder = Recruit::query()->select('id', 'main_file_id', 'company_name', 'ended_at', 'sido', 'gugun')
@@ -25,11 +32,7 @@ class RecruitSearchController extends Controller
                 'file' => function ($query) {
                     $query->select('id', 'url', 'name');
                 }
-            ]);
-
-        if ($sido != 'all') {
-            $builder->where('sido', '=', $sido);
-        }
+            ])->whereIn('sido', $sido);
 
         if ($order == 'newest') {
             $builder->orderBy('created_at', 'desc');
