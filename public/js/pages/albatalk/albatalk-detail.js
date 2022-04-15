cd17839386bfc194fcfd68c3f212239ac8345a44
chdaud78 <qkrri56@naver.com>
2022-04-15 09:48:03 +0900

    [ENH] 알바톡 모바일 구인 정보 페이지 작업

--- public/js/pages/albatalk/albatalk-detail.js
+++ public/js/pages/albatalk/albatalk-detail.js
@@ -43,4 +43,36 @@ $(function() {
             map: map
         });
     });
+
+    // mobile swiper
+    function numFormat(variable) {
+        variable = Number(variable).toString();
+        if (Number(variable) < 10 && variable.length == 1) variable = "0" + variable;
+        return variable;
+    }
+
+    var mySwiper = new Swiper('.swiper-container', {
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        cssMode: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+            renderFraction: function (currentClass, totalClass) {
+                return '<span class="' + currentClass + '"></span>' +
+                    '<span class="pagination-bar"></span>' +
+                    '<span class="' + totalClass + '"></span>';
+            },
+            formatFractionCurrent: function (number) {
+                var Mynumber = numFormat(number)
+                return Mynumber;
+            },
+            formatFractionTotal: function (number) {
+                var Mynumber = numFormat(number)
+                return Mynumber;
+            },
+        },
+    });
 });
--- resources/views/mobile/pages/albatalk/albatalk_detail.blade.php
+++ resources/views/mobile/pages/albatalk/albatalk_detail.blade.php
@@ -2,6 +2,9 @@
 
 @section('script')
     <script type="text/javascript" src="{{ asset('js/pages/albatalk/albatalk-detail.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/swiper-bundle.min.js') }}"></script>
+    <script type="text/javascript"
+            src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId={{ env('NAVER_CLOUD_ID') }}&submodules=geocoder"></script>
 @endsection
 
 @section('vue')
@@ -9,8 +12,6 @@
 @endsection
 
 @section('style')
-    <script type="text/javascript"
-            src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId={{ env('NAVER_CLOUD_ID') }}&submodules=geocoder"></script>
     <link rel="stylesheet" href="{{ mix('css/mobile/pages/albatalk/albatalk-detail.css') }}">
     <link rel="stylesheet" href="{{ mix('css/mobile/pages/albatalk/albatalk-common.css') }}">
 @endsection
@@ -24,5 +25,195 @@
 @endsection
 
 @section('content')
-    <section class="albatalk-recruit-detail-wrap"></section>
+    <section class="albatalk-recruit-detail-wrap">
+        <div class="m-container">
+            <div class="thumbnail-wrap">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <img class="main-thumbnail thumbnail-on" src="{{ $recruit->file->url ?? '' }}"
+                                 alt="치과 사진">
+                        </div>
+                        @if($recruit->file1)
+                            <div class="swiper-slide">
+                                <img class="sub-thumbnail thumbnail-on" src="{{ $recruit->file1->url }}"
+                                     alt="치과 사진">
+                            </div>
+                        @endif
+                        @if($recruit->file2)
+                            <div class="swiper-slide">
+                                <img class="sub-thumbnail thumbnail-on" src="{{ $recruit->file2->url }}"
+                                     alt="치과 사진">
+                            </div>
+                        @endif
+                        @if($recruit->file3)
+                            <div class="swiper-slide">
+                                <img class="sub-thumbnail thumbnail-on" src="{{ $recruit->file3->url }}"
+                                     alt="치과 사진">
+                            </div>
+                        @endif
+                    </div>
+                    <div class="swiper-controller-wrap">
+                        <div class="swiper-pagination"></div>
+                    </div>
+                </div>
+            </div>
+            <section class="office-information-wrap">
+                <div class="m-row">
+                    <div class="office-information">
+                        <p class="ended-date">
+                            모집마감일 : {{ $recruit->ended_at == null? "채용시까지" : $recruit->ended_at->format('n월 d일까지') }}
+                        </p>
+                        <h2>{{$recruit->company_name}}</h2>
+                        <div class="office-content-wrap">
+                            <table>
+                                <tr>
+                                    <th>대표자명</th>
+                                    <td><p>{{$recruit->company_leader}}</p></td>
+                                </tr>
+                                <tr>
+                                    <th>사업자등록번호</th>
+                                    <td><p>{{$recruit->company_license}}</p></td>
+                                </tr>
+                                <tr>
+                                    <th>전화번호</th>
+                                    <td><p>{{$recruit->company_phone}}</p></td>
+                                </tr>
+                                <tr>
+                                    <th>담당자명</th>
+                                    <td><p>{{$recruit->name}}</p></td>
+                                </tr>
+                                <tr>
+                                    <th>담당자 전화번호</th>
+                                    <td><p>{{$recruit->phone}}</p></td>
+                                </tr>
+                                <tr>
+                                    <th>담당자 이메일</th>
+                                    <td><p>{{$recruit->email}}</p></td>
+                                </tr>
+                                <tr>
+                                    <th>홈페이지 주소</th>
+                                    <td><p>{{$recruit->url}}</p></td>
+                                </tr>
+                                <tr>
+                                    <th>주소</th>
+                                    <td>
+                                        <div class="address-wrap">
+                                            <p class="address">서울시 서초구 강남대로79길 59 새로나빌딩 3층</p>
+                                            <a href="" class="btn-map">지도보기</a>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>인근 지하철역</th>
+                                    <td><p>{{$recruit->subway}}</p></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="recruit-detail-information-wrap">
+                <div class="m-row">
+                    <div class="information-title">
+                        <h3>채용 정보</h3>
+                    </div>
+                    <div class="recruit-detail-information-content">
+                        <table>
+                            <tr>
+                                <th>신청분야</th>
+                                <td><p>
+                                        @foreach($applications as $application)
+                                            @if($loop->last)
+                                                {{$application->type}}
+                                            @else
+                                                {{$application->type}},
+                                            @endif
+                                        @endforeach
+                                    </p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>근무형태</th>
+                                <td><p>{{$recruit->typeWork->type}}</p></td>
+                            </tr>
+                            <tr>
+                                <th>직종</th>
+                                <td><p>{{$recruit->typeJob->type}}</p></td>
+                            </tr>
+                            <tr>
+                                <th>급여</th>
+                                @if($salaries[0]->type_salary_id == 4)
+                                    <td><p>{{$salaries[0]->value}}</p></td>
+                                @else
+                                    <td><p>{{$salaries[0]->type}}</p></td>
+                                @endif
+
+                            </tr>
+                            <tr>
+                                <th>학력</th>
+                                <td><p>{{$recruit->typeStudy->type}}</p></td>
+                            </tr>
+                            <tr>
+                                <th>경력</th>
+                                <td><p>신입</p></td>
+                            </tr>
+                            <tr>
+                                <th>근무요일</th>
+                                @if($days[0]->type_day_id == 4)
+                                    <td><p>{{$days[0]->value}}</p></td>
+                                @else
+                                    <td><p>{{$days[0]->type}}</p></td>
+                                @endif
+                            </tr>
+                            <tr>
+                                <th>복리후생</th>
+                                <td>
+                                    <p>
+                                        @foreach($applications as $application)
+                                            @if($loop->last)
+                                                {{$application->type}}
+                                            @else
+                                                {{$application->type}},
+                                            @endif
+                                        @endforeach
+                                    </p>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </section>
+            <section class="recruit-writing">
+                <div class="m-row">
+                    <div class="information-title">
+                        <h3>상세정보</h3>
+                    </div>
+                    <div class="recruit-writing-content">
+                        {!! $recruit->content  !!}
+                    </div>
+                </div>
+            </section>
+        </div>
+        <div class="btn-wrap">
+            <div class="m-row">
+                <button type="submit" class="btn-submit">이력서 제출</button>
+            </div>
+        </div>
+    </section>
+    <section class="popup-area">
+        <div class="dim"></div>
+        <div class="map-popup-wrap popup-wrap">
+            <div class="popup-header">
+                <h4>주소</h4>
+                <a href="#" class="btn-popup-close"></a>
+            </div>
+
+            <input type="hidden" class="map_x" value="127.105399">
+            <input type="hidden" class="map_y" value="37.3595704">
+            <div id="mapzone" class="map"></div>
+
+            <p class="address">서울시 서초구 강남대로79길 59 새로나빌딩 3층</p>
+        </div>
+    </section>
 @endsection
