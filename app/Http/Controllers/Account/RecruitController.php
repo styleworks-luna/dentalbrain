a389a3350efecc0fc2e7f43630bdcf67ac2ce608
Kanginseong <ins1106@onoffmix.com>
2022-04-15 17:29:28 +0900

    [ENH] MICE-2632 마이페이지 구인 정보 구현

--- app/Http/Controllers/Account/RecruitController.php
+++ app/Http/Controllers/Account/RecruitController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
 use App\Models\Recruit\Recruit;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class RecruitController extends Controller
@@ -12,13 +14,16 @@ class RecruitController extends Controller
     {
         $userId = Auth::id();
         $recruits = Recruit::query()->where('user_id', $userId)
-            ->select('id', 'company_name', 'sido', 'gugun', 'started_at', 'ended_at', 'expired_at', 'main_file_id')
+            ->select('id', 'company_name', 'sido', 'gugun', 'started_at', 'ended_at', 'main_file_id', 'expired_at')
             ->with('file:id,url')
             ->orderByDesc('expired_at')
-            ->get();
+            ->get()->map(function ($item, $key) {
+                $item->remain_day = now()->diffInDays($item['expired_at'], false);
+                return $item;
+            });
 
         return response()->json([
-            '$recruits' => $recruits
+            'recruits' => $recruits,
         ]);
     }
 }
