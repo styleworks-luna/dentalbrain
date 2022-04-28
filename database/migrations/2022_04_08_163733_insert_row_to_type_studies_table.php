f1e2c54d54dc24eb18385c8cd126952993d6605a
ndsrhkd <ndsrhkd@onoffmix.com>
2022-04-28 10:20:34 +0900

    [BUG] migration down 에러 수정

--- database/migrations/2022_04_08_163733_insert_row_to_type_studies_table.php
+++ database/migrations/2022_04_08_163733_insert_row_to_type_studies_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class InsertRowToTypeStudiesTable extends Migration
@@ -25,6 +26,11 @@ class InsertRowToTypeStudiesTable extends Migration
      */
     public function down()
     {
+        DB::table('recruits')
+            ->where('type_study_id', '>', 13)
+            ->update(
+                ['type_study_id' => 1]
+            );
         DB::delete("DELETE FROM type_studies WHERE type_studies.id > 13");
         DB::statement("ALTER TABLE type_studies AUTO_INCREMENT = 14");
     }
