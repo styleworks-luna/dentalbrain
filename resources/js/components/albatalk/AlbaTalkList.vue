8ada5cfa0391d64eee9c7509a8fff2089bd8398d
gusrns48 <gusrns48@naver.com>
2022-05-04 13:51:02 +0900

    [ENH] 게재마김일 적용

--- resources/js/components/albatalk/AlbaTalkList.vue
+++ resources/js/components/albatalk/AlbaTalkList.vue
@@ -8,8 +8,8 @@
                         <p class="albatalk-name">{{ list.company_name }}</p>
                         <div class="albatalk-description">
                             <p class="albatalk-place">{{ list.sido }} {{ list.gugun }}</p>
-                            <p v-if="(list.ended_at==null)" class="albatalk-date">채용시까지</p>
-                            <p v-else class="albatalk-date">~ {{ Helper.dateFormatDMW(list.ended_at) }}</p>
+                            <p v-if="(list.expired_at==null)" class="albatalk-date">채용시까지</p>
+                            <p v-else class="albatalk-date">~ {{ Helper.dateFormatDMW(list.expired_at) }}</p>
                         </div>
                     </div>
                 </a>
