27b3695b19af83a4c02f95be2674c92cf6ce18c6
ndsrhkd <ndsrhkd@onoffmix.com>
2022-05-10 10:32:29 +0900

    [ADD] 컨트롤러 추가

---
+++ app/Http/Controllers/Admin/Certificate/QualificationController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Certificate;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class QualificationController extends Controller
+{
+    //
+}
