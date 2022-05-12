6eaa41dce11b47a9e3d072163eb99ddf884c195c
ndsrhkd <ndsrhkd@onoffmix.com>
2022-05-12 11:28:34 +0900

    [ENH] MICE-2749 관리자 강의 증명서 신청 내역 작업

--- app/Http/Controllers/Admin/Certificate/ProgramCertificationController.php
+++ app/Http/Controllers/Admin/Certificate/ProgramCertificationController.php
@@ -4,25 +4,79 @@ namespace App\Http\Controllers\Admin\Certificate;
 
 use App\DTO\Certification\ProgramCertificationDTO;
 use App\Http\Controllers\Controller;
+use App\Models\Certificate\CompletionProfile;
+use App\Models\Certificate\QualificationProfile;
 use App\Models\Program\Program;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class ProgramCertificationController extends Controller
 {
     public function index(Request $request, Program $program)
     {
-        $program->load(['completionProfiles', 'qualificationProfiles']);
-        
-        $number = 1;
-        $collection1 = $program->completionProfiles->map(function ($item) use (&$number) {
-            return ProgramCertificationDTO::create($item, $number++);
-        });
-        $collection2 = $program->qualificationProfiles->map(function ($item) use (&$number) {
-            return ProgramCertificationDTO::create($item, $number++);
-        });
-
-        return response()->json([
-            collect($collection1->values(), $collection2->values())
+        $validated = $request->validate([
+            'category' => ['nullable', 'numeric'],
+            'keyword' => ['nullable', 'string']
         ]);
+        $keyword = $validated['keyword'] ?? null;
+        $category = $validated['category'] ?? null;
+
+        $qualificationQuery = QualificationProfile::query()
+            ->with('user')
+            ->where('program_id', '=', $program->id);
+
+        $completionQuery = CompletionProfile::query()
+            ->with('user')
+            ->where('program_id', '=', $program->id);
+
+        if ($category != null) {
+            $this->searchProfilesByCategory($qualificationQuery, $keyword);
+            $this->searchProfilesByCategory($completionQuery, $keyword);
+        }
+
+        if ($keyword != null) {
+            $this->searchProfilesByKeyword($qualificationQuery, $keyword);
+            $this->searchProfilesByKeyword($completionQuery, $keyword);
+        }
+
+        $number = 1;
+        $result = collect()->concat(
+            $completionQuery->get()->map(function ($item) use (&$number) {
+                return ProgramCertificationDTO::create($item, $number++);
+            })
+        )->concat(
+            $qualificationQuery->get()->map(function ($item) use (&$number) {
+                return ProgramCertificationDTO::create($item, $number++);
+            })
+        );
+
+        return response()->json(
+            $result->toArray()
+        );
     }
+
+    /**
+     * @param Builder $query
+     * @param string $keyword
+     */
+    private function searchProfilesByKeyword(Builder $query, string $keyword): void
+    {
+        $query->where(function ($query) use ($keyword) {
+            $query->orWhere('name', 'LIKE', "%$keyword%")
+                ->orWhere('university', 'LIKE', "%$keyword%")
+                ->orWhereHas('user', function ($query) use ($keyword) {
+                    $query->where('email', 'LIKE', "%$keyword%")
+                        ->orWhere('phone', 'LIKE', "%$keyword%");
+                });
+        });
+    }
+
+    /**
+     * @param Builder $query
+     * @param $keyword
+     */
+    private function searchProfilesByCategory(Builder $query, $keyword): void
+    {
+    }
+
 }
