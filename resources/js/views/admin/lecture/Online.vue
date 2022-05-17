9beb65607f4604aa395e46d029273fb5d67879e4
gusrns48 <gusrns48@onoffmix.com>
2022-05-17 13:56:03 +0900

    [ENH] MICE-2755 증명서 null처리

--- resources/js/views/admin/lecture/Online.vue
+++ resources/js/views/admin/lecture/Online.vue
@@ -53,20 +53,22 @@
                             {{ slotProps.row.title }}
                         </a>
                     </td>
-                    <td>
+                    <td v-if="(slotProps.row.completion_id != null) || (slotProps.row.qualification_id != null)">
                         {{ slotProps.row.certificate_qualification != null ?
                         slotProps.row.certificate_qualification.title : '' }}
                         /
                         {{ slotProps.row.certificate_completion != null ?
                         slotProps.row.certificate_completion.title : '' }}
                     </td>
-                    <td>
+                    <td v-else></td>
+                    <td v-if="(slotProps.row.completion_id != null) || (slotProps.row.qualification_id != null)">
                         {{ slotProps.row.completion_count + slotProps.row.qualification_count }} 명
                         <router-link :to="`/admin/certificate/lecture/${slotProps.row.id}`"
                                      class="btn btn-info ml-2">
                             보기
                         </router-link>
                     </td>
+                    <td v-else></td>
                     <td>
                         {{ slotProps.row.students_count }}명
                         <router-link :to="`/admin/lecture/online/${slotProps.row.id}/student`"
