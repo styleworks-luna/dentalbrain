fdb1fae46d6b14a3c0cca3e4535c8c1d91451cd7
ndsrhkd <ndsrhkd@onoffmix.com>
2022-05-19 14:57:08 +0900

    [ENH] MICE-2768 항목 추가

--- resources/views/excels/certifications.blade.php
+++ resources/views/excels/certifications.blade.php
@@ -10,6 +10,9 @@
         <td>생년월일</td>
         <td>대학교</td>
         <td>학번</td>
+        <td>점수</td>
+        <td>합격여부</td>
+        <td>증명서 발급 상태</td>
     </tr>
     </thead>
     <tbody>
@@ -24,6 +27,28 @@
             <td>{{ \Carbon\Carbon::parse($profile->birthday)->format('Y-m-d') }}</td>
             <td>{{ $profile->university ?? '없음' }}</td>
             <td>{{ $profile->student_number ?? '없음' }}</td>
+            <td>{{ $profile->score }}</td>
+            <td>
+                @switch($profile->status)
+                    @case(\App\Traits\HasCertificateStatus::$DO_NOT_PAID)
+                    결제 전
+                    @break
+                    @case(\App\Traits\HasCertificateStatus::$WAITING)
+                    대기 중
+                    @break
+                    @case(\App\Traits\HasCertificateStatus::$PASS)
+                    합격
+                    @break
+                    @case(\App\Traits\HasCertificateStatus::$FAILED)
+                    불합격
+                    @break
+                    @default
+                    ERROR
+                @endswitch
+            </td>
+            <td>
+                {{ $profile->is_issued ? '발급' : '미발급' }}
+            </td>
         </tr>
     @endforeach
     </tbody>
