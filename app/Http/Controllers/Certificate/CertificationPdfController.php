ae5e326a19997b580724c4c0b65666cfd7f19437
ndsrhkd <ndsrhkd@onoffmix.com>
2022-05-19 10:52:43 +0900

    [ENH] 접근제한 및 예외처리

--- app/Http/Controllers/Certificate/CertificationPdfController.php
+++ app/Http/Controllers/Certificate/CertificationPdfController.php
@@ -8,49 +8,43 @@ use App\Models\Certificate\QualificationProfile;
 use App\Models\Program\Program;
 use App\Models\User;
 use Barryvdh\DomPDF\Facade\Pdf;
-use Dompdf\Options;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 
 class CertificationPdfController extends Controller
 {
-    public function pdfOfCompletion(Program $program, User $user): \Illuminate\Http\Response
+    public function pdfOfCompletion(Program $program, User $user): Response
     {
-        if (Auth::id() != $user->id && Auth::user()->is_admin == false) {
-            throw new ModelNotFoundException();
-        }
+        $this->validateUser($user);
 
         $certificateCompletion = $program->certificateCompletion;
         $profile = CompletionProfile::query()
             ->with('file')
             ->where('program_id', '=', $program->id)
             ->where('user_id', '=', $user->id)
-            ->first();
-        if ($profile == null) {
-            throw new ModelNotFoundException();
-        }
+            ->firstOrFail();
+
+        $this->validateProfile($profile);
 
         return $this->exportCompletionPdf($certificateCompletion, $profile)
             ->stream($program->title . ' ' . $user->name . ' 수료증.pdf');
     }
 
-    public function pdfOfQualification(Program $program, User $user): \Illuminate\Http\Response
+    public function pdfOfQualification(Program $program, User $user): Response
     {
-        if (Auth::id() != $user->id && Auth::user()->is_admin == false) {
-            throw new ModelNotFoundException();
-        }
+        $this->validateUser($user);
 
         $certificateQualification = $program->certificateQualification;
         $profile = QualificationProfile::query()
             ->with('file')
             ->where('program_id', '=', $program->id)
             ->where('user_id', '=', $user->id)
-            ->first();
-        if ($profile == null) {
-            throw new ModelNotFoundException();
-        }
+            ->firstOrFail();
+
+        $this->validateProfile($profile);
+
         return $this->exportQualificationPdf($certificateQualification, $profile)
             ->stream($program->title . ' ' . $user->name . ' 자격증.pdf');
     }
@@ -71,5 +65,28 @@ class CertificationPdfController extends Controller
             'certification' => $certificateCompletion,
             'profile' => $profile
         ]);
+    }
+
+    /**
+     * @param User $user
+     */
+    public function validateUser(User $user): void
+    {
+        if (Auth::id() != $user->id && Auth::user()->is_admin == false) {
+            throw new ModelNotFoundException();
+        }
+    }
+
+    /**
+     * @param Model|CompletionProfile|QualificationProfile $profile
+     */
+    public function validateProfile($profile): void
+    {
+        if (Auth::user()->is_admin == true) {
+            return;
+        }
+        if ($profile->status != QualificationProfile::$PASS || $profile->is_issued == false) {
+            throw new ModelNotFoundException();
+        }
     }
 }
