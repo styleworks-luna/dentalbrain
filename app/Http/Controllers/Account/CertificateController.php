efa1aa34aa8a6ab9e013791a5d7706d529962df1
ndsrhkd <ndsrhkd@onoffmix.com>
2022-05-19 13:18:06 +0900

    [ENH] MICE-2767 발급여부 추가

--- app/Http/Controllers/Account/CertificateController.php
+++ app/Http/Controllers/Account/CertificateController.php
@@ -39,7 +39,8 @@ class CertificateController
                 'files.id as thumbnail_id', 'files.path as thumbnail_path', 'files.url as thumbnail_url',
 
                 'completion_profiles.id as completion_profiles_id', 'completion_profiles.status as completion_status',
-                'qualification_profiles.id as qualification_profiles_id', 'qualification_profiles.status as qualification_status'
+                'qualification_profiles.id as qualification_profiles_id', 'qualification_profiles.status as qualification_status',
+                'qualification_profiles.is_issued'
             )
             ->from('program_students')
             ->leftJoin('payments', 'payments.id', '=', 'program_students.payment_id')
