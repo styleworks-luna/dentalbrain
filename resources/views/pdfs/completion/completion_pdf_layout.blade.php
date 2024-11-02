8aec5685ddaabbd583a9ec338fcc5c99b150d7c1
dannyp0930 <dannyp0930@onoffmix.com>
2024-11-02 13:27:50 +0900

    MICEDEV-17 #time 10m 마진 조정

--- resources/views/pdfs/completion/completion_pdf_layout.blade.php
+++ resources/views/pdfs/completion/completion_pdf_layout.blade.php
@@ -139,7 +139,7 @@
 
     .certification-wrap .certificate-associate span {
         font-size: 33px;
-        margin-left: 10px;
+        margin-left: 5px;
     }
 
     .certification-wrap .certificate-associate .margin-left {
