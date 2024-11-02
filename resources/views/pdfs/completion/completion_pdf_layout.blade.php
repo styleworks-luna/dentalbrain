e5479489054127c9229ed27d82b54b78665fbf87
dannyp0930 <dannyp0930@onoffmix.com>
2024-11-02 13:29:16 +0900

    MICEDEV-17 #time 10m 마진 조정

--- resources/views/pdfs/completion/completion_pdf_layout.blade.php
+++ resources/views/pdfs/completion/completion_pdf_layout.blade.php
@@ -143,7 +143,7 @@
     }
 
     .certification-wrap .certificate-associate .margin-left {
-        margin-left: 30px;
+        margin-left: 100px;
     }
 
     .certification-wrap .certificate-associate span:first-child {
